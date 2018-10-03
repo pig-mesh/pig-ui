40ab216ac28ae8d5f031938898b06e9a3ca4622f
lishangbu <shangshili@hotmail.com>
2018-10-03 17:41:50 +0800

    :lipstick: 更新界面和样式文件:路由管理和客户端管理按钮微调

--- src/const/crud/client.js
+++ src/const/crud/client.js
@@ -20,110 +20,111 @@ const DIC = {
     label: '否',
     value: 'false'
   },
-    {
-      label: '是',
-      value: 'true'
-    }
+  {
+    label: '是',
+    value: 'true'
+  }
   ]
 }
 export const tableOption = {
-  "border": true,
-  "index": true,
-  "stripe": true,
-  "menuAlign": "center",
-  "align": "center",
-  "editBtn": false,
-  "delBtn": false,
-  "dic": [],
-  "column": [{
+  'border': true,
+  'index': true,
+  'stripe': true,
+  'menuAlign': 'center',
+  'align': 'center',
+  'editBtn': false,
+  'delBtn': false,
+  'addBtn': false,
+  'dic': [],
+  'column': [{
     width: 150,
-    label: "编号",
-    prop: "clientId",
+    label: '编号',
+    prop: 'clientId',
     align: 'center',
     sortable: true,
     rules: [{
       required: true,
-      message: "请输入clientId",
-      trigger: "blur"
+      message: '请输入clientId',
+      trigger: 'blur'
     }]
   }, {
     width: 300,
-    label: "密钥",
-    prop: "clientSecret",
+    label: '密钥',
+    prop: 'clientSecret',
     align: 'center',
     sortable: true,
     rules: [{
       required: true,
-      message: "请输入clientSecret",
-      trigger: "blur"
+      message: '请输入clientSecret',
+      trigger: 'blur'
     }]
   }, {
-    label: "域",
-    prop: "scope",
+    label: '域',
+    prop: 'scope',
     align: 'center',
     width: 150,
     rules: [{
       required: true,
-      message: "请输入scope",
-      trigger: "blur"
+      message: '请输入scope',
+      trigger: 'blur'
     }]
   }, {
-    label: "授权模式",
-    prop: "authorizedGrantTypes",
+    label: '授权模式',
+    prop: 'authorizedGrantTypes',
     align: 'center',
     width: 150,
     hide: true,
     rules: [{
       required: true,
-      message: "请输入授权模式",
-      trigger: "blur"
+      message: '请输入授权模式',
+      trigger: 'blur'
     }]
   }, {
-    label: "回调地址",
-    prop: "webServerRedirectUri",
+    label: '回调地址',
+    prop: 'webServerRedirectUri',
     align: 'center',
     width: 150,
-    hide: true,
+    hide: true
   }, {
-    label: "权限",
-    prop: "authorities",
+    label: '权限',
+    prop: 'authorities',
     align: 'center',
     width: 150,
-    hide: true,
+    hide: true
   }, {
-    label: "请求令牌",
-    prop: "accessTokenValidity",
+    label: '请求令牌',
+    prop: 'accessTokenValidity',
     align: 'center',
     width: 150,
-    hide: true,
+    hide: true
   }, {
-    label: "刷新令牌",
-    prop: "refreshTokenValidity",
+    label: '刷新令牌',
+    prop: 'refreshTokenValidity',
     align: 'center',
     width: 150,
-    hide: true,
+    hide: true
   }, {
-    label: "扩展信息",
-    prop: "additionalInformation",
+    label: '扩展信息',
+    prop: 'additionalInformation',
     align: 'center',
     width: 150,
-    hide: true,
+    hide: true
   }, {
-    label: "自动放行",
-    prop: "autoapprove",
+    label: '自动放行',
+    prop: 'autoapprove',
     align: 'center',
     type: 'radio',
-    dicData:DIC.vaild,
+    dicData: DIC.vaild,
     width: 150,
     rules: [{
       required: true,
-      message: "请选择是否放行",
-      trigger: "blur"
+      message: '请选择是否放行',
+      trigger: 'blur'
     }]
-  },{
-    label: "资源ID",
-    prop: "resourceIds",
+  }, {
+    label: '资源ID',
+    prop: 'resourceIds',
     align: 'center',
-    width: 150,
+    width: 150
   }]
 }
--- src/const/crud/route.js
+++ src/const/crud/route.js
@@ -17,28 +17,29 @@
 
 const DIC = {
   vaild: [{
-      label: '否',
-      value: '0'
-    },
-    {
-      label: '是',
-      value: '1'
-    }
+    label: '否',
+    value: '0'
+  },
+  {
+    label: '是',
+    value: '1'
+  }
   ]
 }
 export const tableOption = {
-  "border": true,
-  "index": true,
-  "stripe": true,
-  "menuAlign": "center",
-  "align": "center",
-  "editBtn": false,
-  "delBtn": false,
-  "dic": [],
-  "column": [{
+  'border': true,
+  'index': true,
+  'stripe': true,
+  'menuAlign': 'center',
+  'align': 'center',
+  'editBtn': false,
+  'delBtn': false,
+  'addBtn': false,
+  'dic': [],
+  'column': [{
     width: 150,
-    label: "编号",
-    prop: "id",
+    label: '编号',
+    prop: 'id',
     align: 'center',
     sortable: true,
     hide: true,
@@ -46,78 +47,78 @@ export const tableOption = {
     addVisdiplay: false
   }, {
     width: 150,
-    label: "服务名称",
-    prop: "serviceId",
+    label: '服务名称',
+    prop: 'serviceId',
     align: 'center',
     sortable: true,
     fixed: true,
     rules: [{
       required: true,
-      message: "请输入服务名称",
-      trigger: "blur"
+      message: '请输入服务名称',
+      trigger: 'blur'
     }]
   }, {
-    label: "匹配路径",
-    prop: "path",
+    label: '匹配路径',
+    prop: 'path',
     align: 'center',
     span: 24,
-    width: 150,
+    width: 150
   }, {
-    label: "转发地址",
-    prop: "url",
+    label: '转发地址',
+    prop: 'url',
     span: 24,
     align: 'center',
     width: 150
   }, {
-    label: "去掉前缀",
-    prop: "stripPrefix",
+    label: '去掉前缀',
+    prop: 'stripPrefix',
     align: 'center',
     width: 150,
     type: 'radio',
     dicData: DIC.vaild,
     rules: [{
       required: true,
-      message: "是否去掉前缀",
-      trigger: "blur"
+      message: '是否去掉前缀',
+      trigger: 'blur'
     }]
   }, {
-    label: "是否重试",
-    prop: "retryable",
+    label: '是否重试',
+    prop: 'retryable',
     align: 'center',
     width: 150,
     type: 'radio',
     dicData: DIC.vaild,
     rules: [{
       required: true,
-      message: "是否重试",
-      trigger: "blur"
+      message: '是否重试',
+      trigger: 'blur'
     }]
   }, {
-    label: "是否启用",
-    prop: "enabled",
+    label: '是否启用',
+    prop: 'enabled',
     align: 'center',
     width: 150,
     type: 'radio',
     dicData: DIC.vaild,
-   rules: [{
-     required: true,
-     message: "是否启用",
-     trigger: "blur"
-   }]
+    rules: [{
+      required: true,
+      message: '是否启用',
+      trigger: 'blur'
+    }]
   }, {
-    label: "敏感头",
-    prop: "sensitiveheadersList",
+    label: '敏感头',
+    prop: 'sensitiveheadersList',
     align: 'center',
-    width: 200,
+    width: 200
   }, {
-    label: "创建时间",
-    prop: "createTime",
+    label: '创建时间',
+    prop: 'createTime',
     format: 'YYYY-MM-DD hh:mm:ss',
     valueFormat: 'YYYY-MM-DD hh:mm:ss',
     type: 'date',
     align: 'center',
     editVisdiplay: false,
     addVisdiplay: false,
-    width: 200,
+    width: 200
   }]
 }
