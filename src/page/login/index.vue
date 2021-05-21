f6e2074273ab826cf902ee1833d8d80634f4558b
lbw <wangiegie@163.com>
2021-05-21 16:04:00 +0800

    tag: 3.2.0

--- src/page/login/index.vue
+++ src/page/login/index.vue
@@ -6,7 +6,7 @@
              src="/img/logo.png"
              alt="">
         <p class="title">{{website.infoTitle}}</p>
-        <p>©2021 v3.1.1</p>
+        <p>©2021 v3.2.0</p>
       </div>
       <div class="login-border">
         <div class="login-main">
