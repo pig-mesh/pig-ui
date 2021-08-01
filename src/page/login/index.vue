3114b89142db88ffc744e3fd816fea2387c97f37
lbw <wangiegie@gmail.com>
2021-08-01 15:17:09 +0800

    :bookmark: Releasing / Version tags. 3.2.3

--- src/page/login/index.vue
+++ src/page/login/index.vue
@@ -6,7 +6,7 @@
              src="/img/logo.png"
              alt="">
         <p class="title">{{website.infoTitle}}</p>
-        <p>©2021 v3.2.2</p>
+        <p>©2021 v3.2.3</p>
       </div>
       <div class="login-border">
         <div class="login-main">
