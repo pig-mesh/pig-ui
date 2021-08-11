a2ff2e34cd2bccce4126209bd63ae3aff9b95891
wjie <lydygg@163.com>
2021-08-11 11:28:51 +0800

    feat closed #I44WES 参考 vben 登录页重构现有登录页面UI

--- src/page/login/userlogin.vue
+++ src/page/login/userlogin.vue
@@ -1,65 +1,79 @@
 <template>
-  <el-form class="login-form"
-           status-icon
-           :rules="loginRules"
-           ref="loginForm"
-           :model="loginForm"
-           label-width="0">
+  <el-form
+    class="login-form"
+    status-icon
+    :rules="loginRules"
+    ref="loginForm"
+    :model="loginForm"
+    label-width="0"
+  >
     <el-form-item prop="username">
-      <el-input size="small"
-                @keyup.enter.native="handleLogin"
-                v-model="loginForm.username"
-                auto-complete="off"
-                placeholder="请输入用户名">
-        <i slot="prefix"
-           class="icon-yonghu"></i>
+      <el-input
+        size="small"
+        @keyup.enter.native="handleLogin"
+        v-model="loginForm.username"
+        auto-complete="off"
+        placeholder="请输入用户名"
+      >
+        <i slot="prefix" class="icon-yonghu"></i>
       </el-input>
     </el-form-item>
     <el-form-item prop="password">
-      <el-input size="small"
-                @keyup.enter.native="handleLogin"
-                :type="passwordType"
-                v-model="loginForm.password"
-                auto-complete="off"
-                placeholder="请输入密码">
-        <i class="el-icon-view el-input__icon"
-           slot="suffix"
-           @click="showPassword"></i>
-        <i slot="prefix"
-           class="icon-mima"></i>
+      <el-input
+        size="small"
+        @keyup.enter.native="handleLogin"
+        :type="passwordType"
+        v-model="loginForm.password"
+        auto-complete="off"
+        placeholder="请输入密码"
+      >
+        <i
+          class="el-icon-view el-input__icon"
+          slot="suffix"
+          @click="showPassword"
+        ></i>
+        <i slot="prefix" class="icon-mima"></i>
       </el-input>
     </el-form-item>
     <el-form-item prop="code">
       <el-row :span="24">
         <el-col :span="16">
-          <el-input size="small"
-                    @keyup.enter.native="handleLogin"
-                    :maxlength="code.len"
-                    v-model="loginForm.code"
-                    auto-complete="off"
-                    placeholder="请输入验证码">
-            <i slot="prefix"
-               class="icon-yanzhengma"></i>
+          <el-input
+            size="small"
+            @keyup.enter.native="handleLogin"
+            :maxlength="code.len"
+            v-model="loginForm.code"
+            auto-complete="off"
+            placeholder="请输入验证码"
+          >
+            <i slot="prefix" class="icon-yanzhengma"></i>
           </el-input>
         </el-col>
         <el-col :span="8">
           <div class="login-code">
-            <span class="login-code-img"
-                  @click="refreshCode"
-                  v-if="code.type == 'text'">{{code.value}}</span>
-            <img :src="code.src"
-                 class="login-code-img"
-                 @click="refreshCode"
-                 v-else/>
+            <span
+              class="login-code-img"
+              @click="refreshCode"
+              v-if="code.type == 'text'"
+              >{{ code.value }}</span
+            >
+            <img
+              :src="code.src"
+              class="login-code-img"
+              @click="refreshCode"
+              v-else
+            />
           </div>
         </el-col>
       </el-row>
-
     </el-form-item>
     <el-form-item>
-      <el-button type="primary"
-                 @click.native.prevent="handleLogin"
-                 class="login-submit">登录</el-button>
+      <el-button
+        type="primary"
+        @click.native.prevent="handleLogin"
+        class="login-submit"
+        >登录</el-button
+      >
     </el-form-item>
   </el-form>
 </template>
@@ -69,8 +83,7 @@ import { randomLenNum } from "@/util/util";
 import { mapGetters } from "vuex";
 export default {
   name: "userlogin",
-  data () {
-
+  data() {
     return {
       loginForm: {
         username: "admin",
@@ -93,43 +106,41 @@ export default {
           { required: true, message: "请输入密码", trigger: "blur" },
           { min: 6, message: "密码长度最少为6位", trigger: "blur" }
         ],
-        code: [
-          { required: true, message: "请输入验证码", trigger: "blur" }
-        ]
+        code: [{ required: true, message: "请输入验证码", trigger: "blur" }]
       },
       passwordType: "password"
     };
   },
-  created () {
+  created() {
     this.refreshCode();
   },
-  mounted () { },
   computed: {
     ...mapGetters(["tagWel"])
   },
-  props: [],
   methods: {
-    refreshCode () {
-      this.loginForm.code = ''
-      this.loginForm.randomStr = randomLenNum(this.code.len, true)
-      this.code.type === 'text'
+    refreshCode() {
+      this.loginForm.code = "";
+      this.loginForm.randomStr = randomLenNum(this.code.len, true);
+      this.code.type === "text"
         ? (this.code.value = randomLenNum(this.code.len))
-        : (this.code.src = `${this.codeUrl}?randomStr=${this.loginForm.randomStr}`)
+        : (this.code.src = `${this.codeUrl}?randomStr=${this.loginForm.randomStr}`);
     },
-    showPassword () {
-      this.passwordType == ''
-        ? (this.passwordType = 'password')
-        : (this.passwordType = '')
+    showPassword() {
+      this.passwordType == ""
+        ? (this.passwordType = "password")
+        : (this.passwordType = "");
     },
-    handleLogin () {
+    handleLogin() {
       this.$refs.loginForm.validate(valid => {
         if (valid) {
-          this.$store.dispatch("LoginByUsername", this.loginForm).then(() => {
-            this.$router.push({ path: this.tagWel.value });
-          }).catch(()=>{
-            this.refreshCode()
-          })
-
+          this.$store
+            .dispatch("LoginByUsername", this.loginForm)
+            .then(() => {
+              this.$router.push({ path: this.tagWel.value });
+            })
+            .catch(() => {
+              this.refreshCode();
+            });
         }
       });
     }
@@ -137,5 +148,4 @@ export default {
 };
 </script>
 
-<style>
-</style>
+<style></style>
