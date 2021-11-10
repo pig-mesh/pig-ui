2832e8be148039960971135554377df1f86cde85
MicLng <81649078@qq.com>
2021-11-10 16:47:06 +0800

    :bug: Fixing a bug. close #I4HP7C 令牌管理 >> 数据为空

--- src/views/admin/token/index.vue
+++ src/views/admin/token/index.vue
@@ -29,71 +29,77 @@
                  @current-change="currentChange"
                  @refresh-change="refreshChange"
                  @row-del="rowDel">
+        <template slot="id" slot-scope="scope">
+          <span v-if="scope.row.user_info">{{ scope.row.user_info.id }}</span>
+        </template>
+        <template slot="username" slot-scope="scope">
+          <span v-if="scope.row.user_info">{{ scope.row.user_info.username }}</span>
+        </template>
       </avue-crud>
     </basic-container>
   </div>
 </template>
 
 <script>
-  import {delObj, fetchList} from '@/api/admin/token'
-  import {tableOption} from '@/const/crud/admin/token'
-  import {mapGetters} from 'vuex'
+import {delObj, fetchList} from '@/api/admin/token'
+import {tableOption} from '@/const/crud/admin/token'
+import {mapGetters} from 'vuex'
 
-  export default {
-    name: 'token',
-    data() {
+export default {
+  name: 'token',
+  data() {
+    return {
+      tableData: [],
+      page: {
+        total: 0, // 总页数
+        currentPage: 1, // 当前页数
+        pageSize: 20 // 每页显示多少条
+      },
+      tableLoading: false,
+      tableOption: tableOption
+    }
+  },
+  computed: {
+    ...mapGetters(['permissions']),
+    permissionList() {
       return {
-        tableData: [],
-        page: {
-          total: 0, // 总页数
-          currentPage: 1, // 当前页数
-          pageSize: 20 // 每页显示多少条
-        },
-        tableLoading: false,
-        tableOption: tableOption
-      }
-    },
-    computed: {
-      ...mapGetters(['permissions']),
-      permissionList() {
-        return {
-          delBtn: this.vaildData(this.permissions.sys_token_del, false),
-        }
-      }
-    },
-    methods: {
-      getList(page, params) {
-        this.tableLoading = true
-        fetchList(Object.assign({
-          current: page.currentPage,
-          size: page.pageSize
-        }, params)).then(response => {
-          this.tableData = response.data.data.records
-          this.page.total = response.data.data.total
-          this.tableLoading = false
-        })
-      },
-      rowDel: function (row, index) {
-        this.$confirm('是否强制' + row.username + '下线?', '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning'
-        }).then(function () {
-          return delObj(row.access_token)
-        }).then(data => {
-          this.$message.success('删除成功')
-        })
-      },
-      refreshChange() {
-        this.getList(this.page)
-      },
-      sizeChange(pageSize) {
-        this.page.pageSize = pageSize
-      },
-      currentChange(current) {
-        this.page.currentPage = current
+        delBtn: this.vaildData(this.permissions.sys_token_del, false),
       }
     }
+  },
+  methods: {
+    getList(page, params) {
+      this.tableLoading = true
+      fetchList(Object.assign({
+        current: page.currentPage,
+        size: page.pageSize
+      }, params)).then(response => {
+        this.tableData = response.data.data.records
+        this.page.total = response.data.data.total
+        this.tableLoading = false
+      })
+    },
+    rowDel: function (row, index) {
+      this.$confirm('是否强制' + row.username + '下线?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(function () {
+        return delObj(row.access_token)
+      }).then(data => {
+        this.$message.success('删除成功')
+      })
+    },
+    refreshChange() {
+      this.getList(this.page)
+    },
+    sizeChange(pageSize) {
+      this.page.pageSize = pageSize
+    },
+    currentChange(current) {
+      this.page.currentPage = current
+    }
   }
+}
 </script>
 
