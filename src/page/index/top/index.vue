00cf07bbfde1105a8bab4be55b03a1c0f2b21e9b
lbw <wangiegie@163.com>
2022-10-24 10:36:19 +0800

    :bug:   fix #I5X77B 特定情况下前端头像异常加载

--- src/page/index/top/index.vue
+++ src/page/index/top/index.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="avue-top">
     <div class="top-bar__left">
-      <div class="avue-breadcrumb"
-           :class="[{ 'avue-breadcrumb--active': isCollapse }]"
-           v-if="setting.collapse&&!isHorizontal">
-        <i class="icon-navicon"
-           @click="setCollapse"></i>
+      <div class="avue-breadcrumb" :class="[{ 'avue-breadcrumb--active': isCollapse }]"
+        v-if="setting.collapse&&!isHorizontal">
+        <i class="icon-navicon" @click="setCollapse"></i>
       </div>
     </div>
     <div class="top-bar__title">
@@ -14,21 +12,17 @@
       </div>
     </div>
     <div class="top-bar__right">
-      <div v-if="setting.lock"
-           class="top-bar__item">
+      <div v-if="setting.lock" class="top-bar__item">
         <top-lock></top-lock>
       </div>
-      <div class="top-bar__item"
-           v-if="setting.fullscren">
+      <div class="top-bar__item" v-if="setting.fullscren">
         <top-full></top-full>
       </div>
-      <div class="top-bar__item"
-           v-if="setting.debug">
+      <div class="top-bar__item" v-if="setting.debug">
         <top-logs></top-logs>
       </div>
       <div class="top-user">
-        <img class="top-bar__img"
-             :src="baseUrl + userInfo.avatar || '/img/head.png'">
+        <img class="top-bar__img" :src="userInfo.avatar ? baseUrl + userInfo.avatar : '/img/head.png'">
         <el-dropdown>
           <span class="el-dropdown-link">
             {{ userInfo.username }}
@@ -41,8 +35,7 @@
               <el-dropdown-item>
                 <router-link to="/info/index">{{ $t('navbar.userinfo') }}</router-link>
               </el-dropdown-item>
-              <el-dropdown-item @click="logout"
-                                divided>{{ $t('navbar.logOut') }}
+              <el-dropdown-item @click="logout" divided>{{ $t('navbar.logOut') }}
               </el-dropdown-item>
             </el-dropdown-menu>
           </template>
@@ -53,7 +46,7 @@
   </div>
 </template>
 <script>
-import {mapGetters} from "vuex";
+import { mapGetters } from "vuex";
 import topLock from "./top-lock.vue";
 import topMenu from "./top-menu.vue";
 import topLogs from "./top-logs.vue";
@@ -93,7 +86,7 @@ export default {
         type: "warning"
       }).then(() => {
         this.$store.dispatch("LogOut").then(() => {
-          this.$router.push({path: "/login"});
+          this.$router.push({ path: "/login" });
         });
       });
     }
@@ -102,4 +95,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
 </style>
