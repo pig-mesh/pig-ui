3587597205a0cbcd19e67d694f99399783f5ae94
aeizzz <aeizzz@foxmail.com>
2023-02-14 10:50:43 +0800

    :sparkles: Introducing new features. 增加前段国际化管理并且能从后端获取对应的国际化数据

---
+++ src/api/admin/i18n.ts
@@ -0,0 +1,47 @@
+import request from "/@/utils/request"
+
+export function fetchList(query?: Object) {
+  return request({
+    url: '/admin/i18n/page',
+    method: 'get',
+    params: query
+  })
+}
+
+export function addObj(obj?: Object) {
+  return request({
+    url: '/admin/i18n',
+    method: 'post',
+    data: obj
+  })
+}
+
+export function getObj(id?: string) {
+  return request({
+    url: '/admin/i18n/' + id,
+    method: 'get'
+  })
+}
+
+export function delObj(id?: string) {
+  return request({
+    url: '/admin/i18n/' + id,
+    method: 'delete'
+  })
+}
+
+export function putObj(obj?: Object) {
+  return request({
+    url: '/admin/i18n',
+    method: 'put',
+    data: obj
+  })
+}
+
+
+export function info(){
+  return request({
+    url: '/admin/i18n/info',
+    method: 'get',
+  })
+}
