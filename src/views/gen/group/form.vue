58dd4664856ce3c3c4098323adc52989e854ddc2
dh186609 <5211314yi>
2023-02-23 11:14:56 +0800

    格式化

--- src/views/gen/group/form.vue
+++ src/views/gen/group/form.vue
@@ -1,143 +1,145 @@
 <template>
     <el-dialog :title="form.id ? $t('common.editBtn') : $t('common.addBtn')" v-model="visible">
-      <el-form ref="dataFormRef" :model="form" :rules="dataRules" formDialogRef label-width="90px">
-        <el-row :gutter="24">
-	      <el-col :span="12" class="mb20">
-	        <el-form-item :label="t('group.groupName')" prop="groupName">
-	          <el-input v-model="form.groupName" :placeholder="t('group.inputGroupNameTip')"/>
-	        </el-form-item>
-          </el-col>
-	      <el-col :span="12" class="mb20">
-	        <el-form-item :label="t('group.groupDesc')" prop="groupDesc">
-	          <el-input v-model="form.groupDesc" :placeholder="t('group.inputGroupDescTip')"/>
-	        </el-form-item>
-          </el-col>
-          <el-col :span="12" class="mb20">
-             <el-form-item :label="$t('group.templateType')" prop="templateId">
-                <el-select v-model="form.templateId" :placeholder="$t('group.selectType')" clearable class="w100" multiple>
-                   <el-option v-for="item in templateData" :key="item.id" :label="item.templateName" :value="item.id" />
-                </el-select>
-              </el-form-item>
-          </el-col>
-		</el-row>
-      </el-form>
-      <template #footer>
+        <el-form :model="form" :rules="dataRules" formDialogRef label-width="90px" ref="dataFormRef">
+            <el-row :gutter="24">
+                <el-col :span="12" class="mb20">
+                    <el-form-item :label="t('group.groupName')" prop="groupName">
+                        <el-input :placeholder="t('group.inputGroupNameTip')" v-model="form.groupName"/>
+                    </el-form-item>
+                </el-col>
+                <el-col :span="12" class="mb20">
+                    <el-form-item :label="t('group.groupDesc')" prop="groupDesc">
+                        <el-input :placeholder="t('group.inputGroupDescTip')" v-model="form.groupDesc"/>
+                    </el-form-item>
+                </el-col>
+                <el-col :span="12" class="mb20">
+                    <el-form-item :label="$t('group.templateType')" prop="templateId">
+                        <el-select :placeholder="$t('group.selectType')" class="w100" clearable
+                                   multiple v-model="form.templateId">
+                            <el-option :key="item.id" :label="item.templateName" :value="item.id"
+                                       v-for="item in templateData"/>
+                        </el-select>
+                    </el-form-item>
+                </el-col>
+            </el-row>
+        </el-form>
+        <template #footer>
         <span class="dialog-footer">
           <el-button @click="visible = false">{{ $t('common.cancelButtonText') }}</el-button>
-          <el-button type="primary" @click="onSubmit">{{ $t('common.confirmButtonText') }}</el-button>
+          <el-button @click="onSubmit" type="primary">{{ $t('common.confirmButtonText') }}</el-button>
         </span>
-      </template>
+        </template>
     </el-dialog>
 </template>
 
-<script setup lang="ts" name="GenGroupDialog">
-// 定义子组件向父组件传值/事件
-const emit = defineEmits(['refresh']);
-import { useMessage } from "/@/hooks/message";
-import { getObj, addObj, putObj } from '/@/api/gen/group';
-import { useI18n } from "vue-i18n";
-import { list as templateList } from '/@/api/gen/template'
+<script lang="ts" name="GenGroupDialog" setup>
+    // 定义子组件向父组件传值/事件
+    const emit = defineEmits(['refresh']);
+    import {useMessage} from "/@/hooks/message";
+    import {addObj, getObj, putObj} from '/@/api/gen/group';
+    import {useI18n} from "vue-i18n";
+    import {list as templateList} from '/@/api/gen/template'
 
 
-const { t } = useI18n();
+    const {t} = useI18n();
 
-// 定义变量内容
-const dataFormRef = ref();
-const visible = ref(false)
-const templateData = ref<any[]>([])
-// 定义字典
+    // 定义变量内容
+    const dataFormRef = ref();
+    const visible = ref(false)
+    const templateData = ref<any[]>([])
+    // 定义字典
 
-// 提交表单数据
-const form = reactive({
-    id: '',
-    groupName: '',
-    groupDesc: '',
-    templateId:[] as String [],
-    putList:[]
-});
+    // 提交表单数据
+    const form = reactive({
+        id: '',
+        groupName: '',
+        groupDesc: '',
+        templateId: [] as String [],
+        putList: []
+    });
 
-// 定义校验规则
-const dataRules = ref({
+    // 定义校验规则
+    const dataRules = ref({
         groupName: [{required: true, message: '分组名称不能为空', trigger: 'blur'}],
-        templateId: [{ required: true, message: "模板不能为空", trigger: "blur" }]
-})
+        templateId: [{required: true, message: "模板不能为空", trigger: "blur"}]
+    })
 
-// 打开弹窗
-const openDialog = (id: string) => {
-  visible.value = true
-  form.id = ''
+    // 打开弹窗
+    const openDialog = (id: string) => {
+        visible.value = true
+        form.id = ''
 
-  // 重置表单数据
-  if (dataFormRef.value) {
-    dataFormRef.value.resetFields()
-  }
+        // 重置表单数据
+        if (dataFormRef.value) {
+            dataFormRef.value.resetFields()
+        }
 
-    // 修改获取用户信息
-    if (id) {
-        form.id = id
-        getgenGroupData(id)
-    }
+        // 修改获取用户信息
+        if (id) {
+            form.id = id
+            getgenGroupData(id)
+        }
 
-  // 获取genGroup信息
-  if (id) {
-    form.id = id
-    getgenGroupData(id)
-  }
-    getTemplateData()
-};
+        // 获取genGroup信息
+        if (id) {
+            form.id = id
+            getgenGroupData(id)
+        }
+        getTemplateData()
+    };
 
-// 提交
-const onSubmit = () => {
-  dataFormRef.value.validate((valid: boolean) => {
-    if (!valid) {
-      return false
-    }
+    // 提交
+    const onSubmit = () => {
+        dataFormRef.value.validate((valid: boolean) => {
+            if (!valid) {
+                return false
+            }
 
-    // 更新
-    if (form.id) {
-      putObj(form).then(() => {
-        useMessage().success(t('common.editSuccessText'))
-        visible.value = false // 关闭弹窗
-        emit('refresh')
-      }).catch((err: any) => {
-        useMessage().error(err.msg)
-      })
-    } else {
-      addObj(form).then(() => {
-        useMessage().success(t('common.addSuccessText'))
-        visible.value = false // 关闭弹窗
-        emit('refresh')
-      }).catch((err: any) => {
-        useMessage().error(err.msg)
-      })
-    }
-  })
-}
-
-// 初始化表单数据
-const getgenGroupData = (id: string) => {
-  // 获取数据
-  getObj(id).then((res: any) => {
-    Object.assign(form, res.data)
-      form.templateId=[]
-    if (res.data.templateList) {
-        let list = res.data.templateList
-        list.forEach(item=>{
-            form.templateId.push(item.id)
+            // 更新
+            if (form.id) {
+                putObj(form).then(() => {
+                    useMessage().success(t('common.editSuccessText'))
+                    visible.value = false // 关闭弹窗
+                    emit('refresh')
+                }).catch((err: any) => {
+                    useMessage().error(err.msg)
+                })
+            } else {
+                addObj(form).then(() => {
+                    useMessage().success(t('common.addSuccessText'))
+                    visible.value = false // 关闭弹窗
+                    emit('refresh')
+                }).catch((err: any) => {
+                    useMessage().error(err.msg)
+                })
+            }
         })
     }
-  })
-};
 
-// 角色数据
-const getTemplateData = () => {
-    templateList().then(res => {
-        templateData.value = res.data
-    })
-}
+    // 初始化表单数据
+    const getgenGroupData = (id: string) => {
+        // 获取数据
+        getObj(id).then((res: any) => {
+            Object.assign(form, res.data)
+            form.templateId = []
+            if (res.data.templateList) {
+                let list = res.data.templateList
+                list.forEach(item => {
+                    form.templateId.push(item.id)
+                })
+            }
+        })
+    };
 
-// 暴露变量
-defineExpose({
-  openDialog
-});
+    // 角色数据
+    const getTemplateData = () => {
+        templateList().then(res => {
+            templateData.value = res.data
+        })
+    }
+
+    // 暴露变量
+    defineExpose({
+        openDialog
+    });
 </script>
--- src/views/gen/group/index.vue
+++ src/views/gen/group/index.vue
@@ -1,134 +1,141 @@
 <template>
-  <div class="layout-padding">
-    <el-card class="layout-padding-auto">
-      <el-row v-show="showSearch" class="mb8">
-        <el-form :model="state.queryForm" ref="queryRef" :inline="true">
-      <el-form-item :label="$t('group.id')" prop="id" >
-        <el-input :placeholder="t('group.inputIdTip')" v-model="state.queryForm.id"
-          style="max-width: 180px" />
-      </el-form-item>
-      <el-form-item :label="$t('group.groupName')" prop="groupName" >
-        <el-input :placeholder="t('group.inputGroupNameTip')" v-model="state.queryForm.groupName"
-          style="max-width: 180px" />
-      </el-form-item>
-          <el-form-item class="ml2">
-            <el-button formDialogRef icon="search" type="primary" @click="getDataList">
-              {{ $t('common.queryBtn') }}
-            </el-button>
-            <el-button icon="Refresh" formDialogRef @click="resetQuery">{{ $t('common.resetBtn') }}</el-button>
-          </el-form-item>
-        </el-form>
-      </el-row>
-      <el-row>
-        <div class="mb8" style="width: 100%">
-          <el-button formDialogRef icon="folder-add" type="primary" class="ml10" @click="formDialogRef.openDialog()"
-            v-auth="'codegen_group_add'">
-            {{ $t('common.addBtn') }}
-          </el-button>
-          <el-button formDialogRef icon="Download" type="primary" class="ml10" @click="exportExcel"
-            v-auth="'codegen_group_export'">
-            {{ $t('common.exportBtn') }}
-          </el-button>
-          <el-button formDialogRef :disabled="multiple" icon="Delete" type="primary" class="ml10"
-            v-auth="'codegen_group_del'" @click="handleDelete(selectObjs)">
-            {{ $t('common.delBtn') }}
-          </el-button>
-          <right-toolbar v-model:showSearch="showSearch" class="ml10" style="float: right;margin-right: 20px"
-            @queryTable="getDataList"></right-toolbar>
-        </div>
-      </el-row>
-      <el-table :data="state.dataList" v-loading="state.loading" style="width: 100%"
-        @selection-change="handleSelectionChange" @sort-change="sortChangeHandle">
-        <el-table-column type="selection" width="60" align="center" />
-        <el-table-column type="index" :label="t('group.index')" width="80" />
-          <el-table-column prop="id" :label="t('group.id')"  show-overflow-tooltip/>
-          <el-table-column prop="groupName" :label="t('group.groupName')"  show-overflow-tooltip/>
-          <el-table-column prop="groupDesc" :label="t('group.groupDesc')"  show-overflow-tooltip/>
-        <el-table-column :label="$t('common.action')" width="150">
-          <template #default="scope">
-            <el-button text type="primary" v-auth="'codegen_group_edit'"
-              @click="formDialogRef.openDialog(scope.row.id)">{{ $t('common.editBtn') }}</el-button>
-            <el-button text type="primary" v-auth="'codegen_group_del'" @click="handleDelete([scope.row.id])">{{
-              $t('common.delBtn')
-            }}</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
-      <pagination @size-change="sizeChangeHandle" @current-change="currentChangeHandle" v-bind="state.pagination" />
-    </el-card>
+    <div class="layout-padding">
+        <el-card class="layout-padding-auto">
+            <el-row class="mb8" v-show="showSearch">
+                <el-form :inline="true" :model="state.queryForm" ref="queryRef">
+                    <el-form-item :label="$t('group.id')" prop="id">
+                        <el-input :placeholder="t('group.inputIdTip')" style="max-width: 180px"
+                                  v-model="state.queryForm.id"/>
+                    </el-form-item>
+                    <el-form-item :label="$t('group.groupName')" prop="groupName">
+                        <el-input :placeholder="t('group.inputGroupNameTip')" style="max-width: 180px"
+                                  v-model="state.queryForm.groupName"/>
+                    </el-form-item>
+                    <el-form-item class="ml2">
+                        <el-button @click="getDataList" formDialogRef icon="search" type="primary">
+                            {{ $t('common.queryBtn') }}
+                        </el-button>
+                        <el-button @click="resetQuery" formDialogRef icon="Refresh">{{ $t('common.resetBtn') }}
+                        </el-button>
+                    </el-form-item>
+                </el-form>
+            </el-row>
+            <el-row>
+                <div class="mb8" style="width: 100%">
+                    <el-button @click="formDialogRef.openDialog()" class="ml10" formDialogRef icon="folder-add"
+                               type="primary"
+                               v-auth="'codegen_group_add'">
+                        {{ $t('common.addBtn') }}
+                    </el-button>
+                    <el-button @click="exportExcel" class="ml10" formDialogRef icon="Download" type="primary"
+                               v-auth="'codegen_group_export'">
+                        {{ $t('common.exportBtn') }}
+                    </el-button>
+                    <el-button :disabled="multiple" @click="handleDelete(selectObjs)" class="ml10" formDialogRef
+                               icon="Delete"
+                               type="primary" v-auth="'codegen_group_del'">
+                        {{ $t('common.delBtn') }}
+                    </el-button>
+                    <right-toolbar @queryTable="getDataList" class="ml10" style="float: right;margin-right: 20px"
+                                   v-model:showSearch="showSearch"></right-toolbar>
+                </div>
+            </el-row>
+            <el-table :data="state.dataList" @selection-change="handleSelectionChange" @sort-change="sortChangeHandle"
+                      style="width: 100%" v-loading="state.loading">
+                <el-table-column align="center" type="selection" width="60"/>
+                <el-table-column :label="t('group.index')" type="index" width="80"/>
+                <el-table-column :label="t('group.id')" prop="id" show-overflow-tooltip/>
+                <el-table-column :label="t('group.groupName')" prop="groupName" show-overflow-tooltip/>
+                <el-table-column :label="t('group.groupDesc')" prop="groupDesc" show-overflow-tooltip/>
+                <el-table-column :label="$t('common.action')" width="150">
+                    <template #default="scope">
+                        <el-button @click="formDialogRef.openDialog(scope.row.id)" text type="primary"
+                                   v-auth="'codegen_group_edit'">{{ $t('common.editBtn') }}
+                        </el-button>
+                        <el-button @click="handleDelete([scope.row.id])" text type="primary"
+                                   v-auth="'codegen_group_del'">{{
+                            $t('common.delBtn')
+                            }}
+                        </el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+            <pagination @current-change="currentChangeHandle" @size-change="sizeChangeHandle"
+                        v-bind="state.pagination"/>
+        </el-card>
 
-    <!-- 编辑、新增  -->
-    <form-dialog ref="formDialogRef" @refresh="getDataList()" />
-  </div>
+        <!-- 编辑、新增  -->
+        <form-dialog @refresh="getDataList()" ref="formDialogRef"/>
+    </div>
 </template>
 
-<script setup lang="ts" name="systemGenGroup">
-import { BasicTableProps, useTable } from "/@/hooks/table";
-import { fetchList, delObjs } from "/@/api/gen/group";
-import { useMessage, useMessageBox } from "/@/hooks/message";
-import { useI18n } from "vue-i18n";
+<script lang="ts" name="systemGenGroup" setup>
+    import {BasicTableProps, useTable} from "/@/hooks/table";
+    import {delObjs, fetchList} from "/@/api/gen/group";
+    import {useMessage, useMessageBox} from "/@/hooks/message";
+    import {useI18n} from "vue-i18n";
 
-// 引入组件
-const FormDialog = defineAsyncComponent(() => import('./form.vue'));
-const { t } = useI18n()
-// 定义查询字典
+    // 引入组件
+    const FormDialog = defineAsyncComponent(() => import('./form.vue'));
+    const {t} = useI18n()
+    // 定义查询字典
 
-// 定义变量内容
-const formDialogRef = ref()
-// 搜索变量
-const queryRef = ref()
-const showSearch = ref(true)
-// 多选变量
-const selectObjs = ref([]) as any
-const multiple = ref(true)
+    // 定义变量内容
+    const formDialogRef = ref()
+    // 搜索变量
+    const queryRef = ref()
+    const showSearch = ref(true)
+    // 多选变量
+    const selectObjs = ref([]) as any
+    const multiple = ref(true)
 
-const state: BasicTableProps = reactive<BasicTableProps>({
-  queryForm: {},
-  pageList: fetchList
-})
-
-//  table hook
-const {
-  getDataList,
-  currentChangeHandle,
-  sizeChangeHandle,
-  sortChangeHandle,
-  downBlobFile
-} = useTable(state)
-
-// 清空搜索条件
-const resetQuery = () => {
-  // 清空搜索条件
-  queryRef.value.resetFields()
-  // 清空多选
-  selectObjs.value = []
-  getDataList()
-}
-
-// 导出excel
-const exportExcel = () => {
-  downBlobFile('/codegen/group/export', state.queryForm, 'group.xlsx')
-}
-
-// 多选事件
-const handleSelectionChange = (objs: any) => {
-  selectObjs.value = []
-  objs.forEach((val: any) => {
-    selectObjs.value.push(val.id)
-  });
-  multiple.value = !objs.length
-}
-
-// 删除操作
-const handleDelete = (ids: string[]) => {
-  useMessageBox().confirm(t('common.delConfirmText'))
-    .then(() => {
-      delObjs(ids).then(() => {
-        getDataList();
-        useMessage().success(t('common.delSuccessText'));
-      }).catch((err: any) => {
-        useMessage().error(err.msg)
-      })
+    const state: BasicTableProps = reactive<BasicTableProps>({
+        queryForm: {},
+        pageList: fetchList
     })
-};
+
+    //  table hook
+    const {
+        getDataList,
+        currentChangeHandle,
+        sizeChangeHandle,
+        sortChangeHandle,
+        downBlobFile
+    } = useTable(state)
+
+    // 清空搜索条件
+    const resetQuery = () => {
+        // 清空搜索条件
+        queryRef.value.resetFields()
+        // 清空多选
+        selectObjs.value = []
+        getDataList()
+    }
+
+    // 导出excel
+    const exportExcel = () => {
+        downBlobFile('/codegen/group/export', state.queryForm, 'group.xlsx')
+    }
+
+    // 多选事件
+    const handleSelectionChange = (objs: any) => {
+        selectObjs.value = []
+        objs.forEach((val: any) => {
+            selectObjs.value.push(val.id)
+        });
+        multiple.value = !objs.length
+    }
+
+    // 删除操作
+    const handleDelete = (ids: string[]) => {
+        useMessageBox().confirm(t('common.delConfirmText'))
+            .then(() => {
+                delObjs(ids).then(() => {
+                    getDataList();
+                    useMessage().success(t('common.delSuccessText'));
+                }).catch((err: any) => {
+                    useMessage().error(err.msg)
+                })
+            })
+    };
 </script>
