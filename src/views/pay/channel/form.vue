7710f928b584c01d655580c16aa92d6363fac2ab
dh186609 <5211314yi>
2023-02-28 09:02:28 +0800

    :tada: 支付渠道生成

---
+++ src/views/pay/channel/form.vue
@@ -0,0 +1,196 @@
+<template>
+    <el-dialog :close-on-click-modal="false" :title="form.id ? $t('common.editBtn') : $t('common.addBtn')"
+               draggable v-model="visible">
+        <el-form :model="form" :rules="dataRules" formDialogRef label-width="90px" ref="dataFormRef"
+                 v-loading="loading">
+            <el-row :gutter="24">
+                <el-col :span="12" class="mb20">
+                    <el-form-item :label="t('channel.mchId')" prop="mchId">
+                        <el-input :placeholder="t('channel.inputMchIdTip')" v-model="form.mchId"/>
+                    </el-form-item>
+                </el-col>
+
+                <el-col :span="12" class="mb20">
+                    <el-form-item :label="t('channel.channelId')" prop="channelId">
+                        <el-input :placeholder="t('channel.inputChannelIdTip')" v-model="form.channelId"/>
+                    </el-form-item>
+                </el-col>
+
+                <el-col :span="12" class="mb20">
+                    <el-form-item :label="t('channel.channelName')" prop="channelName">
+                        <el-input :placeholder="t('channel.inputChannelNameTip')" v-model="form.channelName"/>
+                    </el-form-item>
+                </el-col>
+
+                <el-col :span="12" class="mb20">
+                    <el-form-item :label="t('channel.channelMchId')" prop="channelMchId">
+                        <el-input :placeholder="t('channel.inputChannelMchIdTip')" v-model="form.channelMchId"/>
+                    </el-form-item>
+                </el-col>
+
+                <el-col :span="12" class="mb20">
+                    <el-form-item :label="t('channel.returnUrl')" prop="returnUrl">
+                        <el-input :placeholder="t('channel.inputReturnUrlTip')" v-model="form.returnUrl"/>
+                    </el-form-item>
+                </el-col>
+
+                <el-col :span="12" class="mb20">
+                    <el-form-item :label="t('channel.notifyUrl')" prop="notifyUrl">
+                        <el-input :placeholder="t('channel.inputNotifyUrlTip')" v-model="form.notifyUrl"/>
+                    </el-form-item>
+                </el-col>
+
+
+                <el-col :span="12" class="mb20">
+                    <el-form-item :label="t('tenant.state')" prop="state">
+                        <el-radio-group v-model="form.state">
+                            <el-radio :key="index" :label="item.value" border v-for="(item, index) in status_type">{{
+                                item.label }}
+                            </el-radio>
+                        </el-radio-group>
+                    </el-form-item>
+                </el-col>
+
+                <el-col :span="12" class="mb20">
+                    <el-form-item :label="t('channel.param')" prop="param">
+                        <el-input :placeholder="t('channel.inputParamTip')" v-model="form.param"/>
+                    </el-form-item>
+                </el-col>
+
+                <el-col :span="12" class="mb20">
+                    <el-form-item :label="t('channel.remark')" prop="remark">
+                        <el-input :placeholder="t('channel.inputRemarkTip')" v-model="form.remark"/>
+                    </el-form-item>
+                </el-col>
+
+                <el-col :span="12" class="mb20">
+                    <el-form-item :label="t('channel.appId')" prop="appId">
+                        <el-input :placeholder="t('channel.inputAppIdTip')" v-model="form.appId"/>
+                    </el-form-item>
+                </el-col>
+
+            </el-row>
+        </el-form>
+        <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="visible = false">{{ $t('common.cancelButtonText') }}</el-button>
+          <el-button @click="onSubmit" type="primary">{{ $t('common.confirmButtonText') }}</el-button>
+        </span>
+        </template>
+    </el-dialog>
+</template>
+
+<script lang="ts" name="PayChannelDialog" setup>
+    // 定义子组件向父组件传值/事件
+    import {useDict} from "/@/hooks/dict";
+    import {useMessage} from "/@/hooks/message";
+    import {addObj, getObj, putObj} from '/@/api/pay/channel'
+    import {useI18n} from "vue-i18n"
+
+    const emit = defineEmits(['refresh']);
+
+
+    const {t} = useI18n();
+    const {status_type} = useDict('status_type')
+    // 定义变量内容
+    const dataFormRef = ref();
+    const visible = ref(false)
+    const loading = ref(false)
+    // 定义字典
+
+    // 提交表单数据
+    const form = reactive({
+        id: '',
+        mchId: '',
+        channelId: '',
+        channelName: '',
+        channelMchId: '',
+        returnUrl: '',
+        notifyUrl: '',
+        state: '0',
+        param: '',
+        remark: '',
+        appId: '',
+    });
+
+    // 定义校验规则
+    const dataRules = ref({
+        appId: [{required: true, message: 'appId不能为空', trigger: 'blur'}],
+        mchId: [{required: true, message: '商户ID不能为空', trigger: 'blur'}],
+        channelId: [{required: true, message: '渠道ID不能为空', trigger: 'blur'}],
+        channelName: [{required: true, message: '渠道名称不能为空', trigger: 'blur'}],
+        channelMchId: [{required: true, message: '渠道商户ID不能为空', trigger: 'blur'}],
+        returnUrl: [{required: true, message: '前端回调地址不能为空', trigger: 'blur'}],
+        notifyUrl: [{required: true, message: '后端回调地址不能为空', trigger: 'blur'}],
+        state: [{required: true, message: '状态不能为空', trigger: 'blur'}],
+        param: [{required: true, message: '配置参数不能为空', trigger: 'blur'}],
+        remark: [{required: true, message: '备注不能为空', trigger: 'blur'}],
+
+    })
+
+    // 打开弹窗
+    const openDialog = (id: string) => {
+        visible.value = true
+        form.id = ''
+
+        // 重置表单数据
+        if (dataFormRef.value) {
+            dataFormRef.value.resetFields()
+        }
+
+        // 获取payChannel信息
+        if (id) {
+            form.id = id
+            getpayChannelData(id)
+        }
+    };
+
+    // 提交
+    const onSubmit = () => {
+        dataFormRef.value.validate((valid: boolean) => {
+            if (!valid) {
+                return false
+            }
+            // 更新
+            if (form.id) {
+                loading.value = true
+                putObj(form).then(() => {
+                    useMessage().success(t('common.editSuccessText'))
+                    visible.value = false // 关闭弹窗
+                    emit('refresh')
+                }).catch((err: any) => {
+                    useMessage().error(err.msg)
+                }).finally(() => {
+                    loading.value = false
+                })
+            } else {
+                loading.value = true
+                addObj(form).then(() => {
+                    useMessage().success(t('common.addSuccessText'))
+                    visible.value = false // 关闭弹窗
+                    emit('refresh')
+                }).catch((err: any) => {
+                    useMessage().error(err.msg)
+                }).finally(() => {
+                    loading.value = false
+                })
+            }
+        })
+    }
+
+    // 初始化表单数据
+    const getpayChannelData = (id: string) => {
+        // 获取数据
+        loading.value = true
+        getObj(id).then((res: any) => {
+            Object.assign(form, res.data)
+        }).finally(() => {
+            loading.value = false
+        })
+    };
+
+    // 暴露变量
+    defineExpose({
+        openDialog
+    });
+</script>
