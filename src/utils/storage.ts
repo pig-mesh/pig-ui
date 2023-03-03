237edb87d149115c7b60909cfde23e38ed42e79d
aeizzz <aeizzz@foxmail.com>
2023-03-03 16:58:51 +0800

    :bug: Fixing a bug. tagsViewName正则匹配错误，匹配到含en单词

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -8,18 +8,23 @@ import Cookies from 'js-cookie';
  * @method clear 移除全部永久缓存
  */
 export const Local = {
+	// 查看 v2.4.3版本更新日志
+	setKey(key: string) {
+		// @ts-ignore
+		return `${__NEXT_NAME__}:${key}`;
+	},
 	// 设置永久缓存
-	set(key: string, val: any) {
-		window.localStorage.setItem(key, JSON.stringify(val));
+	set<T>(key: string, val: T) {
+		window.localStorage.setItem(Local.setKey(key), JSON.stringify(val));
 	},
 	// 获取永久缓存
 	get(key: string) {
-		let json = <string>window.localStorage.getItem(key);
+		let json = <string>window.localStorage.getItem(Local.setKey(key));
 		return JSON.parse(json);
 	},
 	// 移除永久缓存
 	remove(key: string) {
-		window.localStorage.removeItem(key);
+		window.localStorage.removeItem(Local.setKey(key));
 	},
 	// 移除全部永久缓存
 	clear() {
@@ -36,25 +41,24 @@ export const Local = {
  */
 export const Session = {
 	// 设置临时缓存
-	set(key: string, val: any) {
-		if (key === 'token' || key === 'refresh_token') return Cookies.set(key, val);
-		window.sessionStorage.setItem(key, JSON.stringify(val));
+	set<T>(key: string, val: T) {
+		if (key === 'token') return Cookies.set(key, val);
+		window.sessionStorage.setItem(Local.setKey(key), JSON.stringify(val));
 	},
 	// 获取临时缓存
 	get(key: string) {
-		if (key === 'token' || key === 'refresh_token') return Cookies.get(key);
-		let json = <string>window.sessionStorage.getItem(key);
+		if (key === 'token') return Cookies.get(key);
+		let json = <string>window.sessionStorage.getItem(Local.setKey(key));
 		return JSON.parse(json);
 	},
 	// 移除临时缓存
 	remove(key: string) {
-		if (key === 'token' || key === 'refresh_token') return Cookies.remove(key);
-		window.sessionStorage.removeItem(key);
+		if (key === 'token') return Cookies.remove(key);
+		window.sessionStorage.removeItem(Local.setKey(key));
 	},
 	// 移除全部临时缓存
 	clear() {
 		Cookies.remove('token');
-		Cookies.remove('refresh_token');
 		window.sessionStorage.clear();
 	},
 };
