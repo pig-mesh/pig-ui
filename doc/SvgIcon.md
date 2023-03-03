87b31f9cce3b35aad378ebec80e6fbf37c19f028
aeizzz <aeizzz@foxmail.com>
2023-03-03 17:54:24 +0800

    :sparkles: Introducing new features. 图标使用文件 个人页面bug修改

---
+++ doc/SvgIcon.md
@@ -0,0 +1,48 @@
+
+## 全局组件
+ - 组件支持`四种`svg图标
+ - `四种`图标均支持 color 和 size 属性，使用示例见下文：
+
+```mermaid
+# font-awesome的图标，使用 fa 作为前缀（带个空格）
+<SvgIcon name="fa fa-pencil" />
+
+# element-plus 的图标，使用 el-icon 作为前缀，图标名称请使用：首字母大写的驼峰语法
+<SvgIcon name="ele-icon-Close" color="#8595F4" size="20" />
+
+# 本地`/src/assets/icons`文件夹内的SVG图标，使用 local 作为前缀，文件名作为后缀
+# 文件自动加载，新增请重新编译
+<SvgIcon name="local-logo" />
+
+# 阿里iconfont的图标，使用 iconfont 作为前缀（带个空格）
+<SvgIcon name="iconfont icon-user" size="20" />
+```
+
+有时，Icon组件会覆盖不了图标的原有颜色或大小，请检查SVG文件的源代码。
+
+
+## 图标添加与配置
+### 本地图标
+1. 将SVG图标文件，放入本地`/src/assets/icons`文件夹，然后重新编译项目。
+2. 系统会自动加载该目录下的所有图标文件备用，现在你可以使用`Icon`组件来显示图标了。
+3. 示例：`<SvgIcon name="local-SVG的文件名" />`
+4. 不建议在此文件夹放置`非常大、非常多`的文件，可能会影响系统加载速度，当确有此方面需求时，请将文件放置到其他位置，并单独导入和使用图标。
+
+### font-awesome 图标
+
+1. 此图标库目前 `pigx-ui` 已经默认加载。加载代码在 `/src/utils/setIconfont.ts`中的cssUrls中。
+2. 你可以直接使用font-awesome 4.7.0的所有图标，这些图标可以在这里 https://fontawesome.com.cn/faicons/ 找到，当然你也可以直接使用图标选择器寻找图标。
+3. 示例：<SvgIcon name="fa fa-pencil" />，注意图标名称一定是以fa加一个空格开头
+
+
+
+### element-plus 图标
+1. 此图标库目前 `pigx-ui` 已经默认加载，加载代码在 `/src/main.ts` 执行的 elementIcons 方法中。
+2. 你可以直接使用 `element-plus/icons-vue ^2.0.10` 的所有图标，在使用 `element-plus` 图标时，请使用`ele` 作为前缀，图标名称请使用：首字母大写的驼峰语法。
+3. 示例：<SvgIcon name="ele-RefreshRight" />
+
+
+### iconfont 图标
+
+1. 目前系统未使用任何iconfont图标，你可以获取iconfont图标库项目的Font class链接后，设置到 `/src/utils/setIconfont.ts` 中的 `cssUrls` 中，系统会自动加载链接中的所有图标以供使用。
+2. 示例：`<SvgIcon name="iconfont icon-user" />`，注意图标名称一定是以iconfont加一个空格开头，以图标名称结尾。
--- src/views/admin/user/personal.vue
+++ src/views/admin/user/personal.vue
@@ -1,130 +1,124 @@
 <template>
-  <el-drawer v-model="visible" :title="$t('personal.name')" size="50%">
-    <el-tabs style="height: 200px" class="demo-tabs">
-      <el-tab-pane label="基本信息">
-        <el-card shadow="hover" class="layout-padding-auto">
-          <el-form :model="formData" :rules="ruleForm" label-width="100px" class="mt35 mb35" ref="formdataRef">
-          <el-row :gutter="20">
-            <el-col :span="24" class="mb20">
-              <el-form-item label="用户名" prop="avatar">
-                <image-upload class="h100 personal-user-left-upload" @change="handleAvatarSuccess">
-                  <img v-if="formData.avatar" :src="formData.avatar" class="avatar" />
-                  <el-icon v-else class="avatar-uploader-icon">
-                    <Plus />
-                  </el-icon>
-                </image-upload>
-              </el-form-item>
-            </el-col>
-            <el-col :span="24" class="mb20">
-              <el-form-item label="用户名" prop="username">
-                <el-input v-model="formData.username" clearable disabled></el-input>
-              </el-form-item>
-            </el-col>
-            <el-col :span="24" class="mb20">
-              <el-form-item label="手机" prop="phone">
-                <el-input v-model="formData.phone" placeholder="请输入手机" clearable></el-input>
-              </el-form-item>
-            </el-col>
+	<el-drawer v-model="visible" :title="$t('personal.name')" size="50%">
+		<el-tabs style="height: 200px" class="demo-tabs">
+			<el-tab-pane label="基本信息">
+				<el-card shadow="hover" class="layout-padding-auto">
+					<el-form :model="formData" :rules="ruleForm" label-width="100px" class="mt35 mb35" ref="formdataRef">
+						<el-row :gutter="20">
+							<el-col :span="24" class="mb20">
+								<el-form-item label="用户名" prop="avatar">
+									<image-upload class="h100 personal-user-left-upload" @change="handleAvatarSuccess">
+										<img v-if="formData.avatar" :src="formData.avatar" class="avatar" />
+										<el-icon v-else class="avatar-uploader-icon">
+											<Plus />
+										</el-icon>
+									</image-upload>
+								</el-form-item>
+							</el-col>
+							<el-col :span="24" class="mb20">
+								<el-form-item label="用户名" prop="username">
+									<el-input v-model="formData.username" clearable disabled></el-input>
+								</el-form-item>
+							</el-col>
+							<el-col :span="24" class="mb20">
+								<el-form-item label="手机" prop="phone">
+									<el-input v-model="formData.phone" placeholder="请输入手机" clearable></el-input>
+								</el-form-item>
+							</el-col>
 
-            <el-col :span="24" class="mb20">
-              <el-form-item label="邮箱" prop="email">
-                <el-input v-model="formData.email" placeholder="请输入邮箱" clearable></el-input>
-              </el-form-item>
-            </el-col>
-            <el-col :span="24" class="mb20">
-              <el-form-item label="昵称" prop="nickname">
-                <el-input v-model="formData.nickname" placeholder="请输入昵称" clearable></el-input>
-              </el-form-item>
-            </el-col>
-            <el-col :span="24" class="mb20">
-              <el-form-item label="姓名" prop="name">
-                <el-input v-model="formData.name" placeholder="请输入姓名" clearable></el-input>
-              </el-form-item>
-            </el-col>
-            <el-col :span="24" class="mb20">
-              <el-form-item label="社交登录" prop="social" >
-                <a-row style="display: flex;justify-content: space-between;width: 300px">
-                  <div @click="handleClick('wechat')" class="item">
-                    <SvgIcon name="local-wechat" :size="20"/>
-                    <p class="title">微信</p>
-                  </div>
-                  <div @click="handleClick('tencent')" class="item">
-                    <SvgIcon name="local-qq" :size="20"/>
-                    <p class="title">QQ</p>
-                  </div>
-                  <div @click="handleClick('gitee')" class="item">
-                    <SvgIcon name="local-gitee" :size="20"/>
-                    <p class="title">Gitee</p>
-                  </div>
-                  <div @click="handleClick('osc')" class="item">
-                    <SvgIcon name="local-oschina" :size="20"/>
-                    <p class="title">开源中国</p>
-                  </div>
-                </a-row>
-              </el-form-item>
-            </el-col>
+							<el-col :span="24" class="mb20">
+								<el-form-item label="邮箱" prop="email">
+									<el-input v-model="formData.email" placeholder="请输入邮箱" clearable></el-input>
+								</el-form-item>
+							</el-col>
+							<el-col :span="24" class="mb20">
+								<el-form-item label="昵称" prop="nickname">
+									<el-input v-model="formData.nickname" placeholder="请输入昵称" clearable></el-input>
+								</el-form-item>
+							</el-col>
+							<el-col :span="24" class="mb20">
+								<el-form-item label="姓名" prop="name">
+									<el-input v-model="formData.name" placeholder="请输入姓名" clearable></el-input>
+								</el-form-item>
+							</el-col>
+							<el-col :span="24" class="mb20">
+								<el-form-item label="社交登录" prop="social">
+									<el-row style="display: flex; justify-content: space-between; width: 300px">
+										<div @click="handleClick('wechat')" class="item">
+											<SvgIcon name="local-wechat" :size="20" />
+											<p class="title">微信</p>
+										</div>
+										<div @click="handleClick('tencent')" class="item">
+											<SvgIcon name="local-qq" :size="20" />
+											<p class="title">QQ</p>
+										</div>
+										<div @click="handleClick('gitee')" class="item">
+											<SvgIcon name="local-gitee" :size="20" />
+											<p class="title">Gitee</p>
+										</div>
+										<div @click="handleClick('osc')" class="item">
+											<SvgIcon name="local-oschina" :size="20" />
+											<p class="title">开源中国</p>
+										</div>
+									</el-row>
+								</el-form-item>
+							</el-col>
 
-            <el-col :span="24" class="mb20">
-              <el-form-item>
-                <el-button type="primary" @click="handleSaveUser">
-                  更新个人信息
-                </el-button>
-              </el-form-item>
-            </el-col>
-          </el-row>
-        </el-form>
-        </el-card>
-      </el-tab-pane>
-      <el-tab-pane label="安全信息">
-        <el-card shadow="hover" class="layout-padding-auto">
-          <el-form :model="passwordFormData" :rules="passwordRuleForm" label-width="100px" class="mt35 mb35" ref="passwordFormdataRef">
-            <el-row :gutter="20">
-              <el-col :span="24" class="mb20">
-                <el-form-item label="原密码" prop="password">
-                  <el-input v-model="passwordFormData.password" placeholder="请输入密码" clearable
-                            type="password"></el-input>
-                </el-form-item>
-              </el-col>
-              <el-col :span="24" class="mb20">
-                <el-form-item label="新密码" prop="newpassword1">
-                  <el-input v-model="passwordFormData.newpassword1" clearable type="password"></el-input>
-                </el-form-item>
-              </el-col>
-              <el-col :span="24" class="mb20">
-                <el-form-item label="确认密码" prop="newpassword2">
-                  <el-input v-model="passwordFormData.newpassword2" clearable type="password"></el-input>
-                </el-form-item>
-              </el-col>
-              <el-col :span="24" class="mb20">
-                <el-form-item>
-                  <el-button type="primary" @click="handleChangePassword">
-                    修改密码
-                  </el-button>
-                </el-form-item>
-              </el-col>
-            </el-row>
-          </el-form>
-        </el-card>
-      </el-tab-pane>
-    </el-tabs>
-  </el-drawer>
+							<el-col :span="24" class="mb20">
+								<el-form-item>
+									<el-button type="primary" @click="handleSaveUser"> 更新个人信息 </el-button>
+								</el-form-item>
+							</el-col>
+						</el-row>
+					</el-form>
+				</el-card>
+			</el-tab-pane>
+			<el-tab-pane label="安全信息">
+				<el-card shadow="hover" class="layout-padding-auto">
+					<el-form :model="passwordFormData" :rules="passwordRuleForm" label-width="100px" class="mt35 mb35" ref="passwordFormdataRef">
+						<el-row :gutter="20">
+							<el-col :span="24" class="mb20">
+								<el-form-item label="原密码" prop="password">
+									<el-input v-model="passwordFormData.password" placeholder="请输入密码" clearable type="password"></el-input>
+								</el-form-item>
+							</el-col>
+							<el-col :span="24" class="mb20">
+								<el-form-item label="新密码" prop="newpassword1">
+									<el-input v-model="passwordFormData.newpassword1" clearable type="password"></el-input>
+								</el-form-item>
+							</el-col>
+							<el-col :span="24" class="mb20">
+								<el-form-item label="确认密码" prop="newpassword2">
+									<el-input v-model="passwordFormData.newpassword2" clearable type="password"></el-input>
+								</el-form-item>
+							</el-col>
+							<el-col :span="24" class="mb20">
+								<el-form-item>
+									<el-button type="primary" @click="handleChangePassword"> 修改密码 </el-button>
+								</el-form-item>
+							</el-col>
+						</el-row>
+					</el-form>
+				</el-card>
+			</el-tab-pane>
+		</el-tabs>
+	</el-drawer>
 </template>
 
 <script setup lang="ts" name="personal">
 import { useUserInfo } from '/@/stores/userInfo';
-import { editInfo, password } from '/@/api/admin/user'
-import { useMessage } from "/@/hooks/message";
-import { rule } from "/@/utils/validate";
+import { editInfo, password } from '/@/api/admin/user';
+import { useMessage } from '/@/hooks/message';
+import { rule } from '/@/utils/validate';
 import other from '/@/utils/other';
-import {Session} from "/@/utils/storage";
-import { useI18n } from "vue-i18n";
+import { Session } from '/@/utils/storage';
+import { useI18n } from 'vue-i18n';
 
-const { t } = useI18n()
+const { t } = useI18n();
 
+const ImageUpload = defineAsyncComponent(() => import('/@/components/Upload/Image.vue'));
 
-const ImageUpload = defineAsyncComponent(() => import('/@/components/Upload/Image.vue'))
-
-const visible = ref(false)
+const visible = ref(false);
 
 // 定义变量内容
 const formData = reactive({
@@ -137,122 +131,124 @@ const formData = reactive({
 });
 
 const passwordFormData = reactive({
-  password: '',
-  newpassword1: '',
-  newpassword2: ''
-})
+	password: '',
+	newpassword1: '',
+	newpassword2: '',
+});
 
-const formdataRef = ref()
-const passwordFormdataRef = ref()
+const formdataRef = ref();
+const passwordFormdataRef = ref();
 
 const ruleForm = reactive({
-
-	phone: [{ required: true, message: "手机号不能为空", trigger: "blur" }, { validator: rule.validatePhone, trigger: 'blur' }],
-	nickname: [{ required: true, message: "昵称不能为空", trigger: "blur" }],
-	email: [{ required: true, message: "邮箱不能为空", trigger: "blur" }],
-	name: [{ required: true, message: "姓名不能为空", trigger: "blur" }],
-
-})
-const validatorPassword2 = (rule: any, value: any, callback: any) =>  {
-  if(value !== passwordFormData.newpassword1){
-    callback(new Error(t('personal.passwordRule')))
-  }else{
-    callback()
-  }
-}
+	phone: [
+		{ required: true, message: '手机号不能为空', trigger: 'blur' },
+		{ validator: rule.validatePhone, trigger: 'blur' },
+	],
+	nickname: [{ required: true, message: '昵称不能为空', trigger: 'blur' }],
+	email: [{ required: true, message: '邮箱不能为空', trigger: 'blur' }],
+	name: [{ required: true, message: '姓名不能为空', trigger: 'blur' }],
+});
+const validatorPassword2 = (rule: any, value: any, callback: any) => {
+	if (value !== passwordFormData.newpassword1) {
+		callback(new Error(t('personal.passwordRule')));
+	} else {
+		callback();
+	}
+};
 
 const passwordRuleForm = reactive({
-  password: [{ required: true, message: "密码不能为空", trigger: "blur" }],
-  newpassword1: [{
-    min: 6,
-    max: 20,
-    message: "用户密码长度必须介于 6 和 20 之间",
-    trigger: "blur"
-  }],
-  newpassword2: [
-    {
-      min: 6,
-      max: 20,
-      message: "用户密码长度必须介于 6 和 20 之间",
-      trigger: "blur"
-    },{ validator: validatorPassword2, trigger: 'blur' }
-  ]
-})
-
+	password: [{ required: true, message: '密码不能为空', trigger: 'blur' }],
+	newpassword1: [
+		{
+			min: 6,
+			max: 20,
+			message: '用户密码长度必须介于 6 和 20 之间',
+			trigger: 'blur',
+		},
+	],
+	newpassword2: [
+		{
+			min: 6,
+			max: 20,
+			message: '用户密码长度必须介于 6 和 20 之间',
+			trigger: 'blur',
+		},
+		{ validator: validatorPassword2, trigger: 'blur' },
+	],
+});
 
 // 头像上传成功
 const handleAvatarSuccess = (url: any) => {
 	formData.avatar = url;
-}
+};
 
 const handleChangePassword = () => {
-  passwordFormdataRef.value.validate((valid: boolean) => {
-    if (!valid) {
-      return false
-    }
-    password(passwordFormData).then(() => {
-      useMessage().success("修改成功")
-      // 需要重新登录
-      // 清除缓存/token等
-      Session.clear();
-      // 使用 reload 时，不需要调用 resetRoute() 重置路由
-      window.location.reload();
-    }).catch(err => {
-      useMessage().error(err.msg)
-    })
-  })
-}
-
+	passwordFormdataRef.value.validate((valid: boolean) => {
+		if (!valid) {
+			return false;
+		}
+		password(passwordFormData)
+			.then(() => {
+				useMessage().success('修改成功');
+				// 需要重新登录
+				// 清除缓存/token等
+				Session.clear();
+				// 使用 reload 时，不需要调用 resetRoute() 重置路由
+				window.location.reload();
+			})
+			.catch((err) => {
+				useMessage().error(err.msg);
+			});
+	});
+};
 
 // 保存用户
 const handleSaveUser = () => {
 	formdataRef.value.validate((valid: boolean) => {
 		if (!valid) {
-			return false
+			return false;
 		}
-		editInfo(formData).then(() => {
-			useMessage().success("修改成功")
-			// 更新上下文的 user信息
-			useUserInfo().setUserInfos()
-		}).catch(err => {
-			useMessage().error(err.msg)
-		})
-	})
-
-}
+		editInfo(formData)
+			.then(() => {
+				useMessage().success('修改成功');
+				// 更新上下文的 user信息
+				useUserInfo().setUserInfos();
+			})
+			.catch((err) => {
+				useMessage().error(err.msg);
+			});
+	});
+};
 
 const handleClick = (thirdpart: string) => {
 	let appid, client_id, redirect_uri, url;
-	redirect_uri = encodeURIComponent(
-		window.location.origin + "/#/authredirect"
-	);
-	if (thirdpart === "wechat") {
-		appid = "wxd1678d3f83b1d83a";
+	redirect_uri = encodeURIComponent(window.location.origin + '/#/authredirect');
+	if (thirdpart === 'wechat') {
+		appid = 'wxd1678d3f83b1d83a';
 		url = `https://open.weixin.qq.com/connect/qrconnect?appid=${appid}&redirect_uri=${redirect_uri}&state=WX-BIND&response_type=code&scope=snsapi_login#wechat_redirect`;
-	} else if (thirdpart === "tencent") {
-		client_id = "101322838";
+	} else if (thirdpart === 'tencent') {
+		client_id = '101322838';
 		url = `https://graph.qq.com/oauth2.0/authorize?response_type=code&state=QQ-BIND&client_id=${client_id}&redirect_uri=${redirect_uri}`;
-	} else if (thirdpart === "gitee") {
-		client_id = '0c29cfd9cb1e0037fc837521bc08c1a7483d8fd9b3e123d46beec59a5544a881'
+	} else if (thirdpart === 'gitee') {
+		client_id = '0c29cfd9cb1e0037fc837521bc08c1a7483d8fd9b3e123d46beec59a5544a881';
 		url = `https://gitee.com/oauth/authorize?response_type=code&state=GITEE-BIND&client_id=${client_id}&redirect_uri=${redirect_uri}`;
-	} else if (thirdpart === "osc") {
-		client_id = "neIIqlwGsjsfsA6uxNqD";
+	} else if (thirdpart === 'osc') {
+		client_id = 'neIIqlwGsjsfsA6uxNqD';
 		url = `https://www.oschina.net/action/oauth2/authorize?response_type=code&client_id=${client_id}&state=OSC-BIND&redirect_uri=${redirect_uri}`;
 	}
-	other.openWindow(url, thirdpart, 540, 540)
-}
+	other.openWindow(url, thirdpart, 540, 540);
+};
 
 const open = () => {
-  visible.value = true
-  const data = useUserInfo().userInfos
-  Object.assign(formData, data.user)
-}
+	visible.value = true;
+	const data = useUserInfo().userInfos;
+	Object.assign(formData, data.user);
+};
 
 // 暴露变量
 defineExpose({
-  open,
+	open,
 });
-
 </script>
 
 <style scoped lang="scss">
@@ -461,9 +457,9 @@ defineExpose({
 	height: 100%;
 }
 
-.item{
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
+.item {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
 }
 </style>
