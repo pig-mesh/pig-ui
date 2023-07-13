ec697310660e1f56fa4fd4e9483dba94e13395ba
lbw <wangiegie@163.com>
2023-07-13 22:45:19 +0800

    :recycle: Refactoring code. #5003 menuBarColor 5.1 调节后无效

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,12 @@ import router from './router';
 import { directive } from '/@/directive';
 import { i18n } from '/@/i18n';
 import other from '/@/utils/other';
-import '/@/theme/index.scss';
 
 import ElementPlus from 'element-plus';
+import '/@/theme/tailwind.css';
 import 'element-plus/dist/index.css';
+import '/@/theme/index.scss';
+
 import VForm3 from 'form-designer-plus'; //引入VForm3库
 import 'form-designer-plus/dist/designer.style.css'; //引入VForm3样式
 
