18d896d4ea2b4e89f4af7d3db6fd28148f167999
lbw <wangiegie@163.com>
2023-08-31 14:11:12 +0800

    fix 代码生成查询条件不回显

--- src/views/gen/table/edit.vue
+++ src/views/gen/table/edit.vue
@@ -3,11 +3,11 @@
 		<!-- 属性设置面板 -->
 		<el-tab-pane label="属性设置" name="field">
 			<sc-form-table ref="fieldTable" v-model="fieldList" :hideAdd="true" :hideDelete="true" drag-sort placeholder="暂无数据">
-        <el-table-column label="主键" prop="primaryPk" width="80" show-overflow-tooltip>
-          <template #default="{ row }">
-            <el-checkbox v-model="row.primaryPk" true-label="1" false-label="0" disabled></el-checkbox>
-          </template>
-        </el-table-column>
+				<el-table-column label="主键" prop="primaryPk" width="80" show-overflow-tooltip>
+					<template #default="{ row }">
+						<el-checkbox v-model="row.primaryPk" true-label="1" false-label="0" disabled></el-checkbox>
+					</template>
+				</el-table-column>
 				<el-table-column label="字段名" prop="fieldName" show-overflow-tooltip></el-table-column>
 				<el-table-column label="说明" prop="fieldComment" show-overflow-tooltip>
 					<template #default="{ row }">
@@ -58,14 +58,14 @@
 						<el-checkbox v-model="row.gridSort" true-label="1" false-label="0" :disabled="row.primaryPk === '1'"></el-checkbox>
 					</template>
 				</el-table-column>
-        <el-table-column label="查询显示" prop="gridSort" width="100" show-overflow-tooltip>
-          <template #default="{ row }">
-            <el-checkbox v-model="row.queryItem" true-label="1" false-label="0" :disabled="row.primaryPk === '1'"></el-checkbox>
-          </template>
-        </el-table-column>
+				<el-table-column label="查询显示" prop="gridSort" width="100" show-overflow-tooltip>
+					<template #default="{ row }">
+						<el-checkbox v-model="row.queryItem" true-label="1" false-label="0" :disabled="row.primaryPk === '1'"></el-checkbox>
+					</template>
+				</el-table-column>
 				<el-table-column label="查询表单类型" prop="queryFormType" show-overflow-tooltip>
 					<template #default="{ row }">
-						<el-select v-model="row.queryFormType" placeholder="请选择查询表单类型" :disabled="row.primaryPk === '1'">
+												<el-select v-model="row.queryFormType" placeholder="请选择查询表单类型" :disabled="row.primaryPk === '1'">
 							<el-option v-for="item in queryTypeList" :key="item.value" :label="item.label" :value="item.value" />
 						</el-select>
 					</template>
@@ -268,8 +268,8 @@ const getTable = (dsName: string, tableName: string) => {
 		.then((res) => {
 			tableId.value = res.data.id;
 			fieldList.value = res.data.fieldList.map((item) => {
-				item.queryFormType = propToType[item.fieldType];
-				item.formType = propToType[item.fieldType];
+				item.queryFormType ? item.queryFormType :  propToType[item.fieldType];
+				item.formType ? item.formType : propToType[item.fieldType];
 				return item;
 			});
 		})
@@ -283,14 +283,14 @@ const getFieldTypeList = async () => {
 	// 获取数据
 	const { data } = await list();
 	// 设置属性类型值
-  const typeMap = new Map();
-  data.forEach((item: any) => {
-    const { attrType, columnType } = item;
-    if (!typeMap.has(attrType)) {
-      typeMap.set(attrType, columnType);
-      typeList.value.push({ label: attrType, value: columnType })
-    }
-  });
+	const typeMap = new Map();
+	data.forEach((item: any) => {
+		const { attrType, columnType } = item;
+		if (!typeMap.has(attrType)) {
+			typeMap.set(attrType, columnType);
+			typeList.value.push({ label: attrType, value: columnType });
+		}
+	});
 	// 增加Object类型
 	typeList.value.push({ label: 'Object', value: 'Object' });
 };
