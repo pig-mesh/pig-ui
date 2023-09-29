715745d4e6e45e6005a377a0bfe3d8a7cdd2c3ff
lbw <wangiegie@163.com>
2023-09-29 17:27:10 +0800

    :sparkles: Introducing new features. 前端支持数学图形验证码

--- src/views/login/component/password.vue
+++ src/views/login/component/password.vue
@@ -1,53 +1,73 @@
 <template>
-	<el-form size="large" class="login-content-form" ref="loginFormRef" :rules="loginRules" :model="state.ruleForm" @keyup.enter="handleVerify">
-		<el-form-item class="login-animation1" prop="username">
-			<el-input text :placeholder="$t('password.accountPlaceholder1')" v-model="state.ruleForm.username" clearable autocomplete="off">
-				<template #prefix>
-					<el-icon class="el-input__icon">
-						<ele-User />
-					</el-icon>
-				</template>
-			</el-input>
-		</el-form-item>
-		<el-form-item class="login-animation2" prop="password">
-			<el-input
-				:type="state.isShowPassword ? 'text' : 'password'"
-				:placeholder="$t('password.accountPlaceholder2')"
-				v-model="state.ruleForm.password"
-				autocomplete="off"
-			>
-				<template #prefix>
-					<el-icon class="el-input__icon">
-						<ele-Unlock />
-					</el-icon>
-				</template>
-				<template #suffix>
-					<i
-						class="iconfont el-input__icon login-content-password"
-						:class="state.isShowPassword ? 'icon-yincangmima' : 'icon-xianshimima'"
-						@click="state.isShowPassword = !state.isShowPassword"
-					>
-					</i>
-				</template>
-			</el-input>
-		</el-form-item>
-		<el-form-item class="login-animation4">
-			<el-button type="primary" class="login-content-submit" v-waves @click="handleVerify" :loading="loading">
-				<span>{{ $t('password.accountBtnText') }}</span>
-			</el-button>
-		</el-form-item>
-		<div class="font12 mt30 login-animation4 login-msg">{{ $t('browserMsgText') }}</div>
-	</el-form>
-	<Verify @success="verifySuccess" :mode="'pop'" :captchaType="'blockPuzzle'" :imgSize="{ width: '330px', height: '155px' }" ref="verifyref" />
+  <el-form size="large" class="login-content-form" ref="loginFormRef" :rules="loginRules" :model="state.ruleForm"
+           @keyup.enter="handleVerify">
+    <el-form-item class="login-animation1" prop="username">
+      <el-input text :placeholder="$t('password.accountPlaceholder1')" v-model="state.ruleForm.username" clearable
+                autocomplete="off">
+        <template #prefix>
+          <el-icon class="el-input__icon">
+            <ele-User/>
+          </el-icon>
+        </template>
+      </el-input>
+    </el-form-item>
+    <el-form-item class="login-animation2" prop="password">
+      <el-input
+          :type="state.isShowPassword ? 'text' : 'password'"
+          :placeholder="$t('password.accountPlaceholder2')"
+          v-model="state.ruleForm.password"
+          autocomplete="off"
+      >
+        <template #prefix>
+          <el-icon class="el-input__icon">
+            <ele-Unlock/>
+          </el-icon>
+        </template>
+        <template #suffix>
+          <i
+              class="iconfont el-input__icon login-content-password"
+              :class="state.isShowPassword ? 'icon-yincangmima' : 'icon-xianshimima'"
+              @click="state.isShowPassword = !state.isShowPassword"
+          >
+          </i>
+        </template>
+      </el-input>
+    </el-form-item>
+    <el-form-item class="login-animation2" prop="code" v-if="verifyImageEnable">
+      <el-col :span="15">
+        <el-input text maxlength="4" :placeholder="$t('mobile.placeholder2')" v-model="state.ruleForm.code" clearable
+                  autocomplete="off">
+          <template #prefix>
+            <el-icon class="el-input__icon">
+              <ele-Position/>
+            </el-icon>
+          </template>
+        </el-input>
+      </el-col>
+      <el-col :span="1"></el-col>
+      <el-col :span="8">
+        <img :src="imgSrc" @click="getVerifyImageCode">
+      </el-col>
+    </el-form-item>
+    <el-form-item class="login-animation4">
+      <el-button type="primary" class="login-content-submit" v-waves @click="handleVerify" :loading="loading">
+        <span>{{ $t('password.accountBtnText') }}</span>
+      </el-button>
+    </el-form-item>
+    <div class="font12 mt30 login-animation4 login-msg">{{ $t('browserMsgText') }}</div>
+  </el-form>
+  <Verify @success="verifySuccess" :mode="'pop'" :captchaType="'blockPuzzle'" v-if="verifyEnable"
+          :imgSize="{ width: '330px', height: '155px' }" ref="verifyref"/>
 </template>
 
 <script setup lang="ts" name="password">
-import { reactive, defineAsyncComponent, ref, defineEmits } from 'vue';
-import { useUserInfo } from '/@/stores/userInfo';
-import { useI18n } from 'vue-i18n';
+import {reactive, defineAsyncComponent, ref, defineEmits} from 'vue';
+import {useUserInfo} from '/@/stores/userInfo';
+import {useI18n} from 'vue-i18n';
+import {generateUUID} from "/@/utils/other";
 
 // 使用国际化插件
-const { t } = useI18n();
+const {t} = useI18n();
 
 // 动态加载滑块验证码组件
 const Verify = defineAsyncComponent(() => import('/@/components/Verifition/Verify.vue'));
@@ -57,50 +77,69 @@ const emit = defineEmits(['signInSuccess']); // 声明事件名称
 const loginFormRef = ref(); // 定义LoginForm表单引用
 const loading = ref(false); // 定义是否正在登录中
 const state = reactive({
-	isShowPassword: false, // 是否显示密码
-	ruleForm: {
-		// 表单数据
-		username: 'admin', // 用户名
-		password: '123456', // 密码
-		code: '', // 验证码
-		randomStr: 'blockPuzzle', // 验证码随机数
-	},
+  isShowPassword: false, // 是否显示密码
+  ruleForm: {
+    // 表单数据
+    username: 'admin', // 用户名
+    password: '123456', // 密码
+    code: '', // 验证码
+    randomStr: 'blockPuzzle', // 验证码随机数
+  },
 });
 
 const loginRules = reactive({
-	username: [{ required: true, trigger: 'blur', message: t('password.accountPlaceholder1') }], // 用户名校验规则
-	password: [{ required: true, trigger: 'blur', message: t('password.accountPlaceholder2') }], // 密码校验规则
+  username: [{required: true, trigger: 'blur', message: t('password.accountPlaceholder1')}], // 用户名校验规则
+  password: [{required: true, trigger: 'blur', message: t('password.accountPlaceholder2')}], // 密码校验规则
 });
 
 const verifyref = ref<InstanceType<typeof Verify>>(null); // 定义verify组件引用
 // 是否开启验证码
 const verifyEnable = ref(import.meta.env.VITE_VERIFY_ENABLE === 'true');
+const verifyImageEnable = ref(import.meta.env.VITE_VERIFY_IMAGE_ENABLE === 'true');
+const imgSrc = ref('')
+
+// 调用图形证码进行校验
+const getVerifyImageCode = () => {
+  state.ruleForm.randomStr = generateUUID()
+  imgSrc.value = `${import.meta.env.VITE_API_URL}${import.meta.env.VITE_IS_MICRO == 'false' ? '/admin' : ''}/code/image?randomStr=${state.ruleForm.randomStr}`
+}
 
 // 调用滑块验证码进行校验
 const handleVerify = async () => {
-	const valid = await loginFormRef.value.validate().catch(() => {}); // 表单校验
+  const valid = await loginFormRef.value.validate().catch(() => {
+  }); // 表单校验
 
-	if (valid && verifyEnable.value) {
-		verifyref.value.show(); // 显示验证组件
-	} else if (valid) {
-		onSignIn(); // 调用登录方法
-	}
+  if (valid && verifyEnable.value) {
+    verifyref.value.show(); // 显示验证组件
+  } else if (valid) {
+    onSignIn(); // 调用登录方法
+  }
 };
 
 // 滑块验证码校验成功调用后台登录接口
 const verifySuccess = (params: any) => {
-	state.ruleForm.code = params.captchaVerification; // 获取验证码
-	onSignIn(); // 调用登录方法
+  state.ruleForm.code = params.captchaVerification; // 获取验证码
+  onSignIn(); // 调用登录方法
 };
 
 // 账号密码登录
 const onSignIn = async () => {
-	loading.value = true; // 正在登录中
-	try {
-		await useUserInfo().login(state.ruleForm); // 调用登录方法
-		emit('signInSuccess'); // 触发事件
-	} finally {
-		loading.value = false; // 登录结束
-	}
+  loading.value = true; // 正在登录中
+  try {
+    await useUserInfo().login(state.ruleForm); // 调用登录方法
+    emit('signInSuccess'); // 触发事件
+  } finally {
+    loading.value = false; // 登录结束
+    if (verifyImageEnable.value) {
+      getVerifyImageCode()
+    }
+  }
 };
+
+onMounted(() => {
+  if (verifyImageEnable.value) {
+    getVerifyImageCode()
+  }
+})
+
 </script>
