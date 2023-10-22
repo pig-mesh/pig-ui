cc2f0f71f51655220fe392a67117e2c0fb2d9d38
lbw <wangiegie@163.com>
2023-10-23 01:13:54 +0800

    :recycle: Refactoring code. 重构主页和收银台样式

--- src/views/home/flow-data.vue
+++ src/views/home/flow-data.vue
@@ -1,17 +1,42 @@
 <template>
-  <el-card style="height: 100%">
-    <el-row>
-      <el-col :span="12">
-        <router-link to="/task/pending">
-          <el-statistic :title="$t('home.pendingTask')" :value="state.pendingNum"/>
-        </router-link>
-      </el-col>
-      <el-col :span="12">
-        <router-link to="/task/cc">
-          <el-statistic :title="$t('home.copyTask')" :value="state.copyNum"/>
-        </router-link>
-      </el-col>
-    </el-row>
+  <el-card class="h-full">
+    <div
+        class="relative flex items-center justify-center">
+      <!-- 待办任务 -->
+      <div class="w-auto flex-row items-center">
+        <div class="rounded-full bg-lightPrimary p-3 dark:bg-navy-700">
+                        <span class="flex items-center text-brand-500 dark:text-white">
+                          <svg t="1697994355915" class="icon" viewBox="0 0 1024 1024" version="1.1"
+                               xmlns="http://www.w3.org/2000/svg" p-id="8441" width="32" height="32"><path
+                              d="M404.266667 85.333333v78.933334h236.8V85.333333h78.933333v78.933334h157.866667A39.466667 39.466667 0 0 1 917.333333 203.733333v631.466667A39.466667 39.466667 0 0 1 877.866667 874.666667H167.466667A39.466667 39.466667 0 0 1 128 835.2V203.733333A39.466667 39.466667 0 0 1 167.466667 164.266667h157.866666V85.333333h78.933334zM838.4 361.6H206.933333V795.733333h631.466667V361.6z m-195.904 84.309333l55.808 55.786667-195.370667 195.370667-139.562666-139.562667 55.893333-55.808 83.712 83.754667 139.541333-139.541334h-0.021333z"
+                              fill="#8a8a8a" p-id="8442"></path></svg>
+                        </span>
+        </div>
+      </div>
+      <router-link to="/task/pending">
+        <div class="h-50 ml-4 flex w-auto flex-col justify-center">
+          <p class="font-dm text-sm font-medium text-gray-600">{{ $t('home.pendingTask') }}</p>
+          <h4 class="text-xl font-bold text-navy-700 dark:text-white">{{ state.pendingNum }}</h4>
+        </div>
+      </router-link>
+      <!-- 抄送任务 -->
+      <div class="ml-8 w-auto flex-row items-center">
+        <div class="rounded-full bg-lightPrimary p-3 dark:bg-navy-700">
+                        <span class="flex items-center text-brand-500 dark:text-white">
+<svg t="1697994410210" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="9746"
+     width="32" height="32"><path
+    d="M382.293 692.497a37.956 37.956 0 0 1-32.29 7.373v0.205l-278.118-84.31a39.39 39.39 0 0 1-5.803-73.113l862.14-420.045a39.39 39.39 0 0 1 55.432 45.056L823.228 800.358a39.39 39.39 0 0 1-49.63 27.99l-295.595-89.566a15.428 15.428 0 0 1-12.083-14.814c0-4.437 1.98-8.397 5.12-11.196l356.762-401.135-445.44 380.792h-0.069z m98.031 93.867l54 17.886a15.428 15.428 0 0 1 7.986 23.142l-53.93 81.92a15.428 15.428 0 0 1-28.33-8.533v-99.806a15.428 15.428 0 0 1 20.274-14.61z"
+    p-id="9747" fill="#8a8a8a"></path></svg>
+                        </span>
+        </div>
+      </div>
+      <router-link to="/task/cc">
+        <div class="h-50 ml-4 flex w-auto flex-col justify-center">
+          <p class="font-dm text-sm font-medium text-gray-600">{{ $t('home.copyTask') }}</p>
+          <h4 class="text-xl font-bold text-navy-700 dark:text-white">{{ state.copyNum }}</h4>
+        </div>
+      </router-link>
+    </div>
   </el-card>
 </template>
 
@@ -24,7 +49,7 @@ const state = reactive({
 })
 
 onMounted(async () => {
-  const { data } = await queryTaskData();
+  const {data} = await queryTaskData();
   state.pendingNum = Number.parseInt(data?.pendingNum || 0)
   state.copyNum = Number.parseInt(data?.copyNum || 0)
 });
--- src/views/pay/cd/index.vue
+++ src/views/pay/cd/index.vue
@@ -1,38 +1,86 @@
 <template>
   <div class="layout-padding">
-    <div class="center-container">
-      <el-row>
-        <el-col :span="24">
-          <h2>聚合支付</h2>
-        </el-col>
-      </el-row>
+    <div class="layout-padding-auto layout-padding-view">
 
-      <el-row>
-        <el-col :span="24">
-          <div class="qrcode-container">
-            <vue-qrcode :value="url" :options="state.options"/>
+      <div
+          class="mx-auto mt-11 w-100 transform overflow-hidden rounded-lg bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-110 hover:shadow-lg">
+        <div class="flex items-center justify-center">
+          <svg t="1697991137004" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
+               p-id="4195" width="32" height="32">
+            <path
+                d="M44.8 631.04c24.32 0 44.8 20.48 44.8 44.8v180.48c0 24.32 20.48 44.8 44.8 44.8h180.48c16.64 0 30.72 8.96 38.4 23.04 7.68 14.08 7.68 30.72 0 44.8s-23.04 23.04-38.4 23.04H89.6c-49.92 0-89.6-40.96-89.6-89.6V675.84c0-25.6 20.48-44.8 44.8-44.8zM89.6 495.36c0-17.92 10.24-33.28 23.04-33.28h765.44c7.68 0 15.36 6.4 19.2 16.64 3.84 10.24 3.84 23.04 0 33.28s-11.52 16.64-19.2 16.64H112.64c-12.8 0-23.04-15.36-23.04-33.28zM314.88 89.6h-179.2C110.08 89.6 89.6 110.08 89.6 135.68v180.48c0 24.32-20.48 44.8-44.8 44.8S0 340.48 0 314.88V89.6c0-49.92 40.96-89.6 89.6-89.6h225.28c16.64 0 30.72 8.96 38.4 23.04 7.68 14.08 7.68 30.72 0 44.8-6.4 14.08-21.76 21.76-38.4 21.76z m631.04 271.36c-24.32 0-44.8-20.48-44.8-44.8V135.68c0-24.32-20.48-44.8-44.8-44.8H675.84c-16.64 0-30.72-8.96-38.4-23.04-7.68-14.08-7.68-30.72 0-44.8s23.04-23.04 38.4-23.04h225.28c49.92 0 89.6 40.96 89.6 89.6v225.28c0 25.6-20.48 46.08-44.8 46.08zM764.16 879.36c-67.84 87.04-148.48 60.16-162.56 53.76-32-8.96-43.52-69.12-3.84-87.04 67.84-21.76 128 3.84 170.24 25.6-1.28 3.84-3.84 7.68-3.84 7.68z m70.4-25.6c30.72-53.76 40.96-106.24 40.96-106.24h-99.84v-37.12h119.04v-16.64h-119.04v-57.6h-53.76v57.6H614.4v16.64h107.52v38.4h-93.44v16.64h186.88c0 3.84 0 3.84-3.84 5.12 0 19.2-14.08 47.36-24.32 69.12-138.24-53.76-177.92-21.76-189.44-16.64-92.16 65.28-5.12 147.2 8.96 144.64 97.28 21.76 160-19.2 203.52-70.4 3.84 3.84 5.12 3.84 8.96 3.84 30.72 16.64 172.8 84.48 172.8 84.48v-81.92c-23.04-2.56-101.12-29.44-157.44-49.92z"
+                fill="#0D90FC" p-id="4196"></path>
+          </svg>
+          <h1 class="text-lg font-medium text-gray-700 sm:text-2xl dark:text-gray-200 ml-4">支付宝扫一扫</h1>
+        </div>
+
+
+        <div class="flex justify-center items-center">
+          <vue-qrcode class="h-48 w-full object-cover object-center" :value="url"
+                      :options="state.options"/>
+        </div>
+
+        <div class="bg-white dark:bg-gray-900">
+          <div class="container px-6 py-2 mx-auto">
+            <!-- 聚合支付 -->
+            <div class="mt-6 space-y-8 xl:mt-6" @click="state.type = '0'">
+              <div
+                  :class="{'border-primary': state.type === '0' ,'flex items-center justify-between max-w-2xl px-8 py-4 mx-auto border  border-gray-300 cursor-pointer rounded-xl':true}">
+                <div class="flex items-center">
+                  <svg xmlns="http://www.w3.org/2000/svg"
+                       :class="{'text-primary': state.type === '0','w-5 h-5 sm:h-9 sm:w-9 text-gray-400':true}"
+                       viewBox="0 0 20 20"
+                       fill="currentColor">
+                    <path fill-rule="evenodd"
+                          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                          clip-rule="evenodd"/>
+                  </svg>
+
+                  <div class="flex flex-col items-center mx-5 space-y-1">
+                    <h2 class="text-lg font-medium text-gray-700 sm:text-2xl dark:text-gray-200">聚合支付</h2>
+                    <div class="px-2 text-xs bg-gray-100 text-blue-400 rounded-full sm:px-4 sm:py-1 dark:bg-gray-700 ">
+                      1% 手续费
+                    </div>
+                  </div>
+                </div>
+
+                <h2 :class="{'text-primary': state.type === '0', 'text-2xl font-semibold sm:text-4xl text-gray-400':true}">
+                  {{ state.amount / 100 }}<span
+                    class="text-base font-medium">/元</span>
+                </h2>
+              </div>
+            </div>
+
+            <!-- 普通模式 -->
+            <div class="mt-6 space-y-8 xl:mt-2" @click="state.type = '1'">
+              <div
+                  :class="{'border-primary': state.type === '1' ,'flex items-center justify-between max-w-2xl px-8 py-4 mx-auto border  border-gray-300 cursor-pointer rounded-xl':true}">
+                <div class="flex items-center">
+                  <svg xmlns="http://www.w3.org/2000/svg"
+                       :class="{'text-primary': state.type === '1','w-5 h-5 sm:h-9 sm:w-9 text-gray-400':true}"
+                       viewBox="0 0 20 20"
+                       fill="currentColor">
+                    <path fill-rule="evenodd"
+                          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                          clip-rule="evenodd"/>
+                  </svg>
+
+                  <div class="flex flex-col items-center mx-5 space-y-1">
+                    <h2 class="text-lg font-medium text-gray-700 sm:text-2xl dark:text-gray-200">普通模式</h2>
+                    <div class="px-2 text-xs bg-gray-100 text-blue-400 rounded-full sm:px-4 sm:py-1 dark:bg-gray-700 ">
+                      零手续费
+                    </div>
+                  </div>
+                </div>
+
+                <h2 :class="{'text-primary': state.type === '1', 'text-2xl font-semibold sm:text-4xl text-gray-400':true}">
+                  {{ state.amount / 100 }}<span
+                    class="text-base font-medium">/元</span></h2>
+              </div>
+            </div>
           </div>
-        </el-col>
-      </el-row>
-      <el-row class="mt-4">
-        <el-col :span="24">
-          <h4>支付宝手机客户端扫一扫</h4>
-          <h5>请勿大额支付，无法退还请知悉</h5>
-        </el-col>
-      </el-row>
-      <el-row>
-        <el-col>
-          <el-radio v-model="state.amount" label="1">0.01 元</el-radio>
-          <el-radio v-model="state.amount" label="10">0.1 元</el-radio>
-          <el-radio v-model="state.amount" label="100">1 元</el-radio>
-        </el-col>
-      </el-row>
-      <el-row>
-        <el-col>
-          <el-radio v-model="state.type" label="0">聚合支付</el-radio>
-          <el-radio v-model="state.type" label="1">普通模式</el-radio>
-        </el-col>
-      </el-row>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -42,6 +90,7 @@
 <script setup lang="ts" name="payCd">
 import {useBuyApi} from '/@/api/pay/cd';
 import {Session} from '/@/utils/storage';
+
 // 导入二维码插件
 const VueQrcode = defineAsyncComponent(() => import('@chenfengyuan/vue-qrcode'));
 
@@ -50,8 +99,8 @@ const host = window.location.host;
 
 const url = ref('url');
 const state = reactive({
-  type: '0',
-  amount: '1',
+  type: '0', // 支付方式
+  amount: '1', // 支付金额
   options: {
     height: 300,
     width: 300,
