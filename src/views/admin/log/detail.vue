bfd2195533bd71a06af052fab950bd1a1ce3b136
lbw <wangiegie@163.com>
2023-12-04 20:40:06 +0800

    :lipstick: Updating the UI and style files. 优化日志详情页面

--- src/views/admin/log/detail.vue
+++ src/views/admin/log/detail.vue
@@ -1,28 +1,89 @@
 <template>
-  <el-drawer v-model="visible" title="日志详情" size="50%">
-    <el-descriptions :column="1">
-      <el-descriptions-item :label="$t('syslog.createTime')">{{ data.createTime }}</el-descriptions-item>
-      <el-descriptions-item :label="$t('syslog.createBy')">{{ data.createBy }}</el-descriptions-item>
-      <el-descriptions-item :label="$t('syslog.requestUri')">{{ data.requestUri }}</el-descriptions-item>
-      <el-descriptions-item :label="$t('syslog.title')">{{ data.title }}</el-descriptions-item>
-      <el-descriptions-item :label="$t('syslog.remoteAddr')">{{ data.remoteAddr }}</el-descriptions-item>
-      <el-descriptions-item :label="$t('syslog.method')">{{ data.method }}</el-descriptions-item>
-      <el-descriptions-item :label="$t('syslog.ua')">{{ data.userAgent }}</el-descriptions-item>
-      <el-descriptions-item :label="$t('syslog.serviceId')">{{ data.serviceId }}</el-descriptions-item>
-      <el-descriptions-item :label="$t('syslog.time')">{{ data.time }}/ms</el-descriptions-item>
-      <el-descriptions-item :label="$t('syslog.params')" v-if="data.params">
-        <code-editor v-model="data.params" theme="darcula" height="100" mode="go"></code-editor>
-      </el-descriptions-item>
-      <el-descriptions-item :label="$t('syslog.exception')" v-if="data.exception">
-        <code-editor v-model="data.exception" theme="darcula" height="200" mode="go"></code-editor>
-      </el-descriptions-item>
-    </el-descriptions>
+  <el-drawer v-model="visible" :title="data.title" size="30%">
+    <div class="w-full">
+      <div class="coding inverse-toggle px-5 pt-4 shadow-lg text-gray-100 text-sm font-mono subpixel-antialiased
+              bg-gray-800  pb-6 pt-4 rounded-lg leading-normal overflow-hidden">
+        <div class="top mb-2 flex">
+          <div class="h-3 w-3 bg-red-500 rounded-full"></div>
+          <div class="ml-2 h-3 w-3 bg-orange-300 rounded-full"></div>
+          <div class="ml-2 h-3 w-3 bg-green-500 rounded-full"></div>
+        </div>
+        <div class="mt-4 flex">
+          <span class="text-green-400">{{ $t('syslog.createTime') }}: </span>
+          <p class="flex-1 typing items-center pl-2 whitespace-normal overflow-hidden break-all">
+            {{ data.createTime }}
+            <br>
+          </p>
+        </div>
+        <div class="mt-4 flex">
+          <span class="text-green-400">{{ $t('syslog.createBy') }}: </span>
+          <p class="flex-1 typing items-center pl-2 whitespace-normal overflow-hidden break-all">
+            {{ data.createBy }}
+            <br>
+          </p>
+        </div>
+        <div class="mt-4 flex">
+          <span class="text-green-400">{{ $t('syslog.requestUri') }}: </span>
+          <p class="flex-1 typing items-center pl-2 whitespace-normal overflow-hidden break-all">
+            {{ data.requestUri }}
+            <br>
+          </p>
+        </div>
+        <div class="mt-4 flex">
+          <span class="text-green-400">{{ $t('syslog.remoteAddr') }}: </span>
+          <p class="flex-1 typing items-center pl-2 whitespace-normal overflow-hidden break-all">
+            {{ data.remoteAddr }}
+            <br>
+          </p>
+        </div>
+        <div class="mt-4 flex">
+          <span class="text-green-400">{{ $t('syslog.method') }}: </span>
+          <p class="flex-1 typing items-center pl-2 whitespace-normal overflow-hidden break-all">
+            {{ data.method }}
+            <br>
+          </p>
+        </div>
+        <div class="mt-4 flex">
+          <span class="text-green-400">{{ $t('syslog.serviceId') }}: </span>
+          <p class="flex-1 typing items-center pl-2 whitespace-normal overflow-hidden break-all">
+            {{ data.serviceId }}
+            <br>
+          </p>
+        </div>
+        <div class="mt-4 flex">
+          <span class="text-green-400">{{ $t('syslog.time') }}: </span>
+          <p class="flex-1 typing items-center pl-2 whitespace-normal overflow-hidden break-all">
+            {{ data.time }}/ms
+            <br>
+          </p>
+        </div>
+        <div class="mt-4 flex">
+          <span class="text-green-400">{{ $t('syslog.ua') }}: </span>
+          <p class="flex-1 typing items-center pl-2 whitespace-normal overflow-hidden break-all">
+            {{ data.userAgent }}
+            <br>
+          </p>
+        </div>
+        <div class="mt-4 flex" v-if="data.params">
+          <span class="text-green-400">{{ $t('syslog.params') }}: </span>
+          <p class="flex-1 typing items-center pl-2 whitespace-normal overflow-hidden break-all">
+            {{ data.params }}
+            <br>
+          </p>
+        </div>
+        <div class="mt-4 flex" v-if="data.exception">
+          <span class="text-green-400">{{ $t('syslog.exception') }}: </span>
+          <p class="flex-1 typing items-center pl-2 whitespace-normal overflow-hidden break-all">
+            {{ data.exception }}
+            <br>
+          </p>
+        </div>
+      </div>
+    </div>
   </el-drawer>
 </template>
 
 <script setup lang="ts" name="log-detail">
-const CodeEditor = defineAsyncComponent(() => import('/@/components/CodeEditor/index.vue'));
-
 const visible = ref(false);
 
 const data = reactive({} as any);
