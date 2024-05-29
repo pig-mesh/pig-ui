7794c3303d7c968934eed6f3390ecae766d6fb4a
ccl <1020308280@qq.com>
2024-05-29 15:44:45 +0800

    【轻量级 PR】：参数校验，数据源管理中没有数据时，代码生成列表会发生错误

--- src/views/gen/table/index.vue
+++ src/views/gen/table/index.vue
@@ -92,7 +92,7 @@ onMounted(() => {
 	list().then((res) => {
 		datasourceList.value = res.data;
 		// 默认去第一个数据源
-		state.queryForm.dsName = datasourceList.value[0].name;
+		state.queryForm.dsName =datasourceList.value.length>0?datasourceList.value[0].name:state.queryForm.dsName;
 		getDataList();
 	});
 });
