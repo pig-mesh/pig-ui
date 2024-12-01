60229b2ba89f69f51d19461d66b87873705f0f11
冷冷 <2270033969@qq.com>
2024-12-01 16:12:15 +0800

    fix(锁屏): 锁屏头像不能正确加载 增加baseURL

--- src/layout/lockScreen/index.vue
+++ src/layout/lockScreen/index.vue
@@ -28,7 +28,7 @@
 				<div v-show="state.isShowLoockLogin" class="layout-lock-screen-login">
 					<div class="layout-lock-screen-login-box">
 						<div class="layout-lock-screen-login-box-img">
-							<img :src="formData.avatar" />
+							<img :src="baseURL + formData.avatar" />
 						</div>
 						<div class="layout-lock-screen-login-box-name">{{ formData.username }}</div>
 						<div class="layout-lock-screen-login-box-value">
@@ -200,12 +200,12 @@ const onLockScreenSubmit = async () => {
 	} catch (err: any) {
 		// 捕获异常并将错误提示信息赋值给mes变量
 		mes.value = err.msg;
-    if(err.msg == '用户凭证已过期'){
-      themeConfig.value.isLockScreen = false;
-      themeConfig.value.lockScreenTime = 30;
-      // 将最新的主题配置保存到本地存储中
-      setLocalThemeConfig();
-    }
+		if (err.msg == '用户凭证已过期') {
+			themeConfig.value.isLockScreen = false;
+			themeConfig.value.lockScreenTime = 30;
+			// 将最新的主题配置保存到本地存储中
+			setLocalThemeConfig();
+		}
 	}
 };
 
