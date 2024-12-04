cdbdbb9fe42cd799a8310eed1369a459719d5f60
冷冷 <2270033969@qq.com>
2024-12-04 22:12:01 +0800

    feat(core): 支持disable-devtool 禁用开发者工具

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
-import {createApp} from 'vue';
+import { createApp } from 'vue';
 import pinia from '/@/stores/index';
 import App from './App.vue';
 import router from './router';
-import {directive} from '/@/directive';
-import Components from '/@/components'
-import {properties} from '/@/utils/globalProperties';
-import {i18n} from '/@/i18n';
+import { directive } from '/@/directive';
+import Components from '/@/components';
+import { i18n } from '/@/i18n';
+import { properties } from '/@/utils/globalProperties';
+import { initAntiDebug } from '/@/utils/anti-debug';
+
+// Initialize anti-debug protection
+initAntiDebug();
 
 // 导入全局样式
 import '/@/theme/tailwind.css';
 import '/@/theme/index.scss';
-
 
 const app = createApp(App);
 
@@ -20,8 +23,8 @@ properties(app);
 directive(app);
 
 app
-    .use(pinia) // pinia 存储
-    .use(router) // 路由
-    .use(Components) // 全局引入自定义的组件&第三方的组件
-    .use(i18n) // 国际化
-    .mount('#app');
+	.use(pinia) // pinia 存储
+	.use(router) // 路由
+	.use(Components) // 全局引入自定义的组件&第三方的组件
+	.use(i18n) // 国际化
+	.mount('#app');
