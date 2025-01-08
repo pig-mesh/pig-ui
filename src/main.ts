fa3ed0ba4bad5d984f4c370ffd7c7ef1c5030a71
冷冷 <2270033969@qq.com>
2025-01-08 20:18:22 +0800

    refactor(core): rename anti-debug.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { directive } from '/@/directive';
 import Components from '/@/components';
 import { i18n } from '/@/i18n';
 import { properties } from '/@/utils/globalProperties';
-import { initAntiDebug } from '/@/utils/anti-debug';
+import { initAntiDebug } from './utils/antiDebug';
 
 // Initialize anti-debug protection
 initAntiDebug();
