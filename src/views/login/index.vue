de3b2a22f339c5536da6e884f7f913a9fdefd226
PIG AI <wangiegie@gmail.com>
2025-07-04 18:38:06 +0800

    feat(tenant): enable tenant selection in login view
    - Added a conditional tenant selection component in the login view.
    - Updated the logic to determine tenant selection visibility based on the new environment variable VITE_TENANT_LIST_ENABLE.
    - Removed the deprecated VITE_AUTO_TENANT variable from the .env file.

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -23,6 +23,10 @@
 				<div class="w-full px-5 py-16 md:w-[45%] md:px-12">
 					<div class="mb-16 text-center">
 						<h1 class="text-4xl font-bold text-gray-900 dark:text-white tracking-wide font-['Inter']">{{ getThemeConfig.globalTitle }}</h1>
+						<!-- 租户选择 -->
+						<div class="mt-6" v-if="tenantEnable">
+							<tenant class="shadow-sm" />
+						</div>
 					</div>
 
 					<div class="w-full px-5">
@@ -86,7 +90,7 @@ const route = useRoute();
 const router = useRouter();
 
 // 是否启用租户下拉框显示
-const tenantEnable = ref(import.meta.env.VITE_TENANT_ENABLE !== 'false');
+const tenantEnable = ref(import.meta.env.VITE_TENANT_LIST_ENABLE === 'true');
 
 // 登录方式
 const loginType = ref(LoginTypeEnum.PASSWORD);
