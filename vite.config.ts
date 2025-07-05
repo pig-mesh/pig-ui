7a6cef1898840851c063d78bbf2e3b35ecf7cfef
PIG AI <wangiegie@gmail.com>
2025-07-05 17:02:46 +0800

    feat(vite): allow all hosts to access the server
    - Added the `allowedHosts` configuration to enable access from all domains and IPs in the Vite server setup.

--- vite.config.ts
+++ vite.config.ts
@@ -55,6 +55,7 @@ const viteConfig = defineConfig((mode: ConfigEnv) => {
 			host: '0.0.0.0', // 服务器地址
 			port: env.VITE_PORT as unknown as number, // 服务器端口号
 			open: env.VITE_OPEN === 'true', // 是否自动打开浏览器
+			allowedHosts: true, // 允许所有域名和IP访问
 			hmr: true, // 启用热更新
 			proxy: {
 				'/api': {
