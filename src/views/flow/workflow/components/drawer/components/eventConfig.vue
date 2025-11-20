5a268d12e6cfdb6f8ab3b0f6273b609248f4c724
PIGCLOUD <wangiegie@gmail.com>
2025-11-20 22:41:48 +0800

    feat(flow): 优化表单组件和样式配置
    - 调整 eventConfig.vue 中 el-input 的 rows 属性为动态绑定
    - 将 roleDialog.vue 的 CSS 导入语法从 @use 改为 @import

--- src/views/flow/workflow/components/drawer/components/eventConfig.vue
+++ src/views/flow/workflow/components/drawer/components/eventConfig.vue
@@ -12,7 +12,7 @@ const props = defineProps({
 <template>
   <div>
     <div class="mb-4">节点任务完成推送接口:</div>
-    <el-input type="textarea" show-word-limit maxlength="200" rows="5" :model-value="props.modelValue"
+    <el-input type="textarea" show-word-limit maxlength="200" :rows="5" :model-value="props.modelValue"
               @input="emit('update:modelValue',$event)"
               placeholder="此节点任务完成后，会往目标接口推送事件通知"/>
   </div>
